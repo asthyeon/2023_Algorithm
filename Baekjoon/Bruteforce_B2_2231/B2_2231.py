e9ede2fc350e96f93411167ac2925cbdcb91985e
asthyeon <asthyeon@gmail.com>
2023-08-03 23:23:57 +0900

    0803

--- Baekjoon/Bruteforce_B2_2231/B2_2231.py
+++ Baekjoon/Bruteforce_B2_2231/B2_2231.py
@@ -6,10 +6,22 @@ input = sys.stdin.readline
 N = int(input())
 
 # 가장 작은 생성자 구하기
-for i in range(N):
-    # i 를 문자열로 바꾸고, map으로 자릿수를 하나씩 분리하여 다시 정수형으로 변환
-    # 각 자릿 수를 더하고 i 자체를 더한 값이 N이랑 같다면 생성자이므로 i 출력
-    if N == i + sum(map(int, str(i))):
+for i in range(N):    
+    # 각 자릿수 합
+    num_sum = 0    
+    
+    # 각 자릿수를 구하고 합에 집어넣기
+    j = i
+    while True:
+        if j < 10:
+            num_sum += j % 10
+            break
+        else:
+            num_sum += j % 10
+            j = j // 10
+
+    # 각 자릿수와 숫자 자체를 더해서 생성자가 맞는지 확인
+    if N == i + num_sum:
         print(i)
         # 가장 작은 생성자 출력을 위해 i 값이 나오면 반복문 종료
         break
