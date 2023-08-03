8bf5ea27bfd616c6e5c95ec60dc64a4d0034d68a
asthyeon <asthyeon@gmail.com>
2023-08-03 21:26:34 +0900

    0803

--- Baekjoon/Bruteforce_S4_1018/S4_1018.py
+++ Baekjoon/Bruteforce_S4_1018/S4_1018.py
@@ -5,41 +5,110 @@ input = sys.stdin.readline
 # N 개의 행, M 개의 열
 N, M = map(int, input().split())
 # 배열 형성
-arr = [list(map(str, input().split())) for _ in range(N)]
-# 색칠한 횟수의 최솟값
-min_count = 1000
+arr = [input() for _ in range(N)]
+# 색칠한 횟수 리스트
+color_list = []
 
-# 배열 내에 체스판 형성
-for i in range(N - 7):
-    for j in range(M - 7):
-        # 색칠한 횟수 초기화
+# 배열 내에서 자를 체스판 형성
+# 마지막 열도 포함하기 위해서 -7 을 해주기
+for i in range(N-7):
+    for j in range(M-7):
+
+        # 블랙으로 시작하는 체스판으로 가정
+        # 블랙으로 시작한 칠한 횟수
         count_B = 0
+        # 화이트로 시작한 칠한 횟수
         count_W = 0
-        # 체스판 자르기(8x8)
-        for row in range(i, 8 + i):
-            for col in range(j, 8 + j):
-                # 인덱스가 짝수라면
-                if (row + col) % 2 == 0:
-                    # 블랙으로 시작할 때 칠하는 횟수 계산
-                    if arr[row][col] != 'B':
-                        count_B += 1
-                    # 화이트일 때 칠하는 횟수 계산
-                    if arr[row][col] != 'W':
-                        count_W += 1
-                # 인덱스가 홀수라면
+        # 가로 행 형성
+        for r in range(i, i + 8):
+            # 세로 열 형성
+            for c in range(j, j + 8):
+                # 블랙으로 시작한다면
+                if arr[i][j] == 'B':
+                    if r % 2 == 0:
+                        if c % 2 == 0:
+                            if arr[r][c] != 'B':
+                                count_B += 1
+                        else:
+                            if arr[r][c] != 'W':
+                                count_B += 1
+                    else:
+                        if c % 2 == 0:
+                            if arr[r][c] != 'W':
+                                count_B += 1
+                        else:
+                            if arr[r][c] != 'B':
+                                count_B += 1
+                # 화이트로 시작한다면
                 else:
-                    # 블랙으로 시작할 때 칠하는 횟수 계산
-                    if arr[row][col] != 'B':
-                        count_W += 1
-                    # 화이트로 시작할 때 칠하는 횟수 계산
-                    if arr[row][col] != 'W':
-                        count_B += 1
-        # 더 적게 칠한 횟수로 최솟값 바꿔주기
+                    if r % 2 == 0:
+                        if c % 2 == 0:
+                            if arr[r][c] != 'W':
+                                count_W += 1
+                        else:
+                            if arr[r][c] != 'B':
+                                count_W += 1
+                    else:
+                        if c % 2 == 0:
+                            if arr[r][c] != 'B':
+                                count_W += 1
+                        else:
+                            if arr[r][c] != 'W':
+                                count_W += 1
+        
+        # 0 값을 제외하기 위해 비교
         if count_B > count_W:
-            if min_count > count_W:
-                min_count = count_W
+            color_list.append(count_B)
         else:
-            if min_count > count_B:
-                min_count = count_B
+            color_list.append(count_W)
 
-print(min_count)
+        # 화이트판으로 시작하는 체스판으로 가정
+        # 블랙으로 시작한 칠한 횟수
+        count_B2 = 0
+        # 화이트로 시작한 칠한 횟수
+        count_W2 = 0
+        # 가로 행 형성
+        for r in range(i, i + 8):
+            # 세로 열 형성
+            for c in range(j, j + 8):
+                # 블랙으로 시작한다면
+                if arr[i][j] != 'B':
+                    if r % 2 == 0:
+                        if c % 2 == 0:
+                            if arr[r][c] != 'B':
+                                count_B2 += 1
+                        else:
+                            if arr[r][c] != 'W':
+                                count_B2 += 1
+                    else:
+                        if c % 2 == 0:
+                            if arr[r][c] != 'W':
+                                count_B2 += 1
+                        else:
+                            if arr[r][c] != 'B':
+                                count_B2 += 1
+                # 화이트로 시작한다면
+                else:
+                    if r % 2 == 0:
+                        if c % 2 == 0:
+                            if arr[r][c] != 'W':
+                                count_W2 += 1
+                        else:
+                            if arr[r][c] != 'B':
+                                count_W2 += 1
+                    else:
+                        if c % 2 == 0:
+                            if arr[r][c] != 'B':
+                                count_W2 += 1
+                        else:
+                            if arr[r][c] != 'W':
+                                count_W2 += 1
+        
+        # 0 값을 제외하기 위해 비교
+        if count_B2 > count_W2:
+            color_list.append(count_B2)
+        else:
+            color_list.append(count_W2)
+
+# 최소값 출력
+print(min(color_list))
