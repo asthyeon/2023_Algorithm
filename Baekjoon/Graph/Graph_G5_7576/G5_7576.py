406cafee64663e53bdbf199b1957d86e9cbe09c6
asthyeon <asthyeon@gmail.com>
2023-08-31 23:09:42 +0900

    0831

--- Baekjoon/Graph/Graph_G5_7576/G5_7576.py
+++ Baekjoon/Graph/Graph_G5_7576/G5_7576.py
@@ -1,72 +1,85 @@
 import sys
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
+from collections import deque
 
 '''
 # 모든 토마토가 익는데 걸리는 시간
 1. 1: 익은 토마토, 0: 익지 않은 토마토, -1: 토마토가 들어있지 않은 칸
 2. 처음부터 모든토마토가 익었으면 0 출력
 3. 모두 익지못하는 상황 -1 출력
+@ 풀이
+(1) 토마토 위치를 전부 찾고 좌표 덱에 넣기
+(2) 덱에 들어가 있는 좌표에서 델타탐색으로 bfs 실행
 '''
 
 
-# bfs 함수 생성
-def bfs(sx, sy, box, ans):
-    # 이번 순회에 익은게 있는지 없는지 확인하기 위함
-    tomato = 0
-    for dx, dy in [[0, 1], [1, 0], [0, -1], [-1, 0]]:
-        nx, ny = sx + dx, sy + dy
-        # 벽 생성 및 토마토가 익지 않았을 때
-        if 0 <= nx < N and 0 <= ny < M and box[nx][ny] == 0:
-            # # 인큐
-            # q.append((nx, ny))
-            # 방문 기록
-            box[nx][ny] = box[sx][sy] + 1
-            # 이번 순회에 익은게 있는지 없는지 확인하기 위함
-            tomato += 1
-    return tomato
+# bfs 함수
+def bfs(q, arr):
+    global check
+    # 큐가 비어있지 않다면
+    if q:
+        # 디큐
+        sx, sy = q.popleft()
+        # 델타탐색
+        for dx, dy in [[0, 1], [1, 0], [0, -1], [-1, 0]]:
+            nx, ny = sx + dx, sy + dy
+            # 벽 형성 및 이동할 수 있는 곳이라면
+            if 0 <= nx < N and 0 <= ny < M and arr[nx][ny] == 0:
+                q.append((nx, ny))
+                arr[nx][ny] = arr[sx][sy] + 1
+        check += 1
 
-# 상자의 가로 칸 M, 상자의 세로 칸 N
+
+# 상자 가로 칸: M, 상자 세로 칸: N
 M, N = map(int, input().split())
+# 토마토 배열 받기
+arr = [list(map(int, input().split())) for _ in range(N)]
+# 큐 형성
+q = deque([])
+# 익은 날짜
+day = -1
+# 토마토가 하나라도 익은게 있는지 없는지 확인하기 위한 변수
+check = 0
 
-# 상자 생성
-box = [list(map(int, input().split())) for _ in range(N)]
+# 토마토 찾기
+for x in range(N):
+    for y in range(M):
+        if arr[x][y] == 1:
+            q.append((x, y))
 
-# 모든 시작점을 찾기 위한 i
-i = 0
-while True:
-    i += 1
-    # 전체 순회를 1번 돌 때 하나도 바뀐게 없다면 출력할 조건
-    ans = 0
-    for sx in range(N):
-        for sy in range(M):
-            if box[sx][sy] == i:
-                tomato = bfs(sx, sy, box, ans)
-                # 토마토가 이번 순회에 하나라도 익었다면 ans += 1
-                if tomato > 0:
-                    ans += 1
-    # 토마토가 익을게 더이상 없다면
-    if ans == 0:
-        break
+# bfs 탐색 시작
+while q:
+    bfs(q, arr)
 
-
-# 아예 다 익었거나 다 익지 않았을 때
-if i == 1:
-    # 하나라도 안익은 토마토가 있다면
-    for j in range(N):
-        if 0 in box[j]:
-            print(-1)
+# 토마토가 하나도 익지 않았다면
+if check == 0:
+    # 0 을 셀 변수
+    zero = 0
+    # 모든 토마토가 익어있는 경우 찾기
+    for i in range(N):
+        if day == 0:
             break
-    # 이미 다 익었다면
-    else:
-        print(0)
-# 익은 것들이 있다면
+        for j in range(M):
+            if arr[i][j] == 0:
+                zero += 1
+                break
+    # 모두 익지 못하는 상황이라면
+    if zero != N * M:
+        day = -1
+# 토마토가 하나라도 익었다면
 else:
-    # 하나라도 안익은 토마토가 있다면
-    for j in range(N):
-        if 0 in box[j]:
-            print(-1)
+    for k in range(N):
+        # 안 익은 토마토가 없다면
+        if 0 not in arr[k]:
+            if day < max(arr[k]) - 1:
+                day = max(arr[k]) - 1
+        # 안 익은 토마토가 있다면
+        else:
+            day = -1
             break
-    # 그게 아니라면 최소 일 수 출력
-    else:
-        print(i - 1)
+
+print(day)
+
+
+
