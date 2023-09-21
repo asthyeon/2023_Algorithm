a76d6266d2e903d3b0e3bf645b8a2847891f827d
asthyeon <asthyeon@gmail.com>
2023-09-21 20:51:25 +0900

    0921

--- Baekjoon/Searching/Searching_G1_1300/G1_1300.py
+++ Baekjoon/Searching/Searching_G1_1300/G1_1300.py
@@ -10,16 +10,41 @@ input = sys.stdin.readline
 4. 배열 A 와 B 의 인덱스는 1 부터 시작
 5. N 은 10^5 보다 작거나 같은 자연수
 6. k 는 min(10^9, N^2)보다 작거나 같은 자연수
+@ 풀이
+(1) 이분탐색 사용
+(2) N * N 배열에서 각 칸들의 값은 i * j 이므로 규칙 존재
+ - 임의의 수 보다 작거나 같은 수의 개수를 구하는 식 존재
+ - k 보다 작거나 같은 수는 k 를 각 행으로 나눈 몫. 다만 N 을 초과할 순 없음
+(3) 이분탐색 사용법
+ - 어떤 수를 정하고 그 수보다 작거나 같은 것이 몇 개인지 찾기
+ - 그 개수가 k개라면 어떤 수는 k 번째 수가 됨
+(4) 규칙을 찾는 것이 중요한 느낌
 """
 
 
 # 이분 탐색 함수
-def binary_search(N):
+def binary_search(N, k):
     start = 1
     end = N ** 2
-    cnt = 0
+    result = 0
     while start <= end:
         mid = (start + end) // 2
+        cnt = 0
+
+        # mid 보다 작거나 같은 수의 개수 찾기
+        for i in range(1, N + 1):
+            # mid를 각 행으로 나눈 수와 N 중 작은 수를 더하기
+            cnt += min(mid // i, N)
+
+        # 작거나 같은 수의 개수가 k 보다 크거나 같으면 답을 갱신하고 범위 줄이기
+        if cnt >= k:
+            result = mid
+            end = mid - 1
+        # k 보다 작다면 범위를 넓히기
+        else:
+            start = mid + 1
+
+    return result
 
 
 # 배열의 크기 N
@@ -27,3 +52,5 @@ N = int(input())
 
 # B[k] 의 k
 k = int(input())
+
+print(binary_search(N, k))
