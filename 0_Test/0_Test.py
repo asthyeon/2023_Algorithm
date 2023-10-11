dbad3a973b557c342a2ff89f87361cf36b19f5db
asthyeon <asthyeon@gmail.com>
2023-10-12 01:04:37 +0900

    1011

--- 0_Test/0_Test.py
+++ 0_Test/0_Test.py
@@ -11,57 +11,39 @@ input = sys.stdin.readline
 (1) dfs 로 하나씩 이동하고 bfs 로 나머지 선거구 성립 되는지 확인
 """
 from collections import deque
+sys.setrecursionlimit(10*9)
 
 
 # dfs 함수
-def dfs(n, populations, adj):
-    dfs_pop = 0
-    # 방문 리스트
-    visited = [0] * (N + 1)
-    # 스택 생성
-    stack = []
-    # 선거구의 길이
-    dfs_length = 0
-    # 시작점 방문 표시
-    visited[n] = 1
-    dfs_length += 1
+def dfs(n, populations, adj, stack, passed, dfs_pop, dfs_length):
+    if passed[n] == 1:
+        return
+    # 스택 형성 및 시작점 푸쉬
+    stack.append(n)
+    # 시작점 방문 처리
+    passed[n] = 1
     dfs_pop += populations[n - 1]
-    bfs(visited, populations, dfs_pop, dfs_length, adj)
-    while True:
-        # 인접 구역 탐색
+    dfs_length += 1
+    if bfs(passed, populations, dfs_pop, dfs_length, adj, stack):
         for w in adj[n]:
-            # 방문하지 않았다면
-            if visited[w] == 0:
-                stack.append(n)
-                dfs_pop += populations[n - 1]
-                dfs_length += 1
-                n = w
-                visited[n] = 1
-                break
-        # 인접한 구역을 다 돌았다면
-        else:
-            # 스택이 비어있지 않다면
-            if stack:
-                dfs_pop -= populations[n - 1]
-                dfs_length -= 1
-                n = stack.pop()
-                continue
-            # 스택이 비어있다면 반복 종료
-            else:
-                break
-        bfs(visited, populations, dfs_pop, dfs_length, adj)
+            dfs(w, populations, adj, stack, passed, dfs_pop, dfs_length)
+    else:
+        stack.pop()
+        dfs_pop -= populations[n - 1]
+        dfs_length -= 1
+        passed[n] = 0
 
 
 # bfs 함수
-def bfs(visited, populations, dfs_pop, dfs_length, adj):
+def bfs(passed, populations, dfs_pop, dfs_length, adj, stack):
     global min_pop
     bfs_pop = 0
     start = 0
     # 새로운 방문리스트
-    visited2 = visited[:]
+    visited = passed[:]
     # 방문하지 않은 곳 시작점 찾기
     for i in range(1, N + 1):
-        if visited2[i] == 0:
+        if visited[i] == 0:
             start = i
             break
     if start == 0:
@@ -71,7 +53,7 @@ def bfs(visited, populations, dfs_pop, dfs_length, adj):
     # 큐 생성 및 시작점 인큐
     q = deque([start])
     # 시작점 방문기록
-    visited2[start] = 1
+    visited[start] = 1
     # 큐가 빌 때까지
     while q:
         n = q.popleft()
@@ -79,16 +61,18 @@ def bfs(visited, populations, dfs_pop, dfs_length, adj):
         bfs_length += 1
         for w in adj[n]:
             # 방문하지 않은 곳이라면
-            if visited2[w] == 0:
+            if visited[w] == 0:
                 q.append(w)
-                visited2[w] = 1
+                visited[w] = 1
     # 두 선거구로 나뉘는지 확인
     if dfs_length + bfs_length != N:
-        return
+        return True
     # 두 선거구로 나뉘었다면
-    differ = abs(dfs_pop - bfs_pop)
-    if min_pop > differ:
-        min_pop = differ
+    else:
+        differ = abs(dfs_pop - bfs_pop)
+        if min_pop > differ:
+            min_pop = differ
+        return True
 
 
 # 구역의 개수 N
@@ -113,7 +97,12 @@ for i in range(1, N + 1):
 min_pop = 1000000000
 
 for j in range(1, N + 1):
-    dfs(1, populations, adj)
+    # 지나온 길
+    passed = [0] * (N + 1)
+    stack = []
+    dfs_pop = 0
+    dfs_length = 0
+    dfs(1, populations, adj, stack, passed, dfs_pop, dfs_length)
 
 if min_pop == 1000000000:
     print(-1)
