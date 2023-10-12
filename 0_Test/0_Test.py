e1d4409622141a2bd56e11be77e12081390e2fde
asthyeon <asthyeon@naver.com>
2023-10-12 17:20:50 +0900

    1012

--- 0_Test/0_Test.py
+++ 0_Test/0_Test.py
@@ -8,7 +8,8 @@ input = sys.stdin.readline
 2. 각 선거구는 연결되어 있어야 함
 3. 두 선거구로 나눌 수 없는 경우 -1 출력
 @ 풀이
-(1) dfs 로 하나씩 이동하고 bfs 로 나머지 선거구 성립 되는지 확인
+(1) 조합을 사용하여 선거구를 나누기
+(2) 그래프 탐색 사용(dfs or bfs)
 """
 from collections import deque
 sys.setrecursionlimit(10*9)
@@ -16,17 +17,23 @@ sys.setrecursionlimit(10*9)
 
 # dfs 함수
 def dfs(n, populations, adj, stack, passed, dfs_pop, dfs_length):
+    # 방문한 곳이라면 빠져나오기
     if passed[n] == 1:
         return
     # 스택 형성 및 시작점 푸쉬
     stack.append(n)
     # 시작점 방문 처리
     passed[n] = 1
+    # 1지역 선거구 인구수
     dfs_pop += populations[n - 1]
+    # 1지역 선거구 길이
     dfs_length += 1
-    if bfs(passed, populations, dfs_pop, dfs_length, adj, stack):
-        for w in adj[n]:
-            dfs(w, populations, adj, stack, passed, dfs_pop, dfs_length)
+    # bfs 함수 사용
+    # bfs(passed, populations, dfs_pop, dfs_length, adj, stack)
+    # 탐색
+    for w in adj[n]:
+        dfs(w, populations, adj, stack, passed, dfs_pop, dfs_length)
+    # 탐색을 다 돌았다면 뒤로 가기
     else:
         stack.pop()
         dfs_pop -= populations[n - 1]
@@ -37,11 +44,12 @@ def dfs(n, populations, adj, stack, passed, dfs_pop, dfs_length):
 # bfs 함수
 def bfs(passed, populations, dfs_pop, dfs_length, adj, stack):
     global min_pop
+    # 2지역 선거구 인구수
     bfs_pop = 0
-    start = 0
     # 새로운 방문리스트
     visited = passed[:]
     # 방문하지 않은 곳 시작점 찾기
+    start = 0
     for i in range(1, N + 1):
         if visited[i] == 0:
             start = i
@@ -69,6 +77,7 @@ def bfs(passed, populations, dfs_pop, dfs_length, adj, stack):
         return True
     # 두 선거구로 나뉘었다면
     else:
+        # 최소값으로 교체
         differ = abs(dfs_pop - bfs_pop)
         if min_pop > differ:
             min_pop = differ
@@ -100,8 +109,11 @@ for j in range(1, N + 1):
     # 지나온 길
     passed = [0] * (N + 1)
     stack = []
+    # 1지역 선거구 인구수
     dfs_pop = 0
+    # 1지역 선거구 길이
     dfs_length = 0
+    # dfs 함수 사용
     dfs(1, populations, adj, stack, passed, dfs_pop, dfs_length)
 
 if min_pop == 1000000000:
