6baacef3350f7496ed3a5d377a2cc9dcd62d1296
asthyeon <asthyeon@gmail.com>
2023-10-19 21:54:59 +0900

    1019

--- 0_Test/0_Test.py
+++ 0_Test/0_Test.py
@@ -12,37 +12,39 @@ input = sys.stdin.readline
 6. 김지민과 임한수는 서로 만나기 전까지 항상 이김
 7. 서로 대결하지 않으면 -1 출력
 """
-sys.setrecursionlimit(10**9)
 
 
 # 토너먼트 함수
-def tournament(N, players, kim, lim, number):
-    # 다음 라운드에 올라갈 인원
-    match = []
-    # 다음 라운드에 붙일 번호
-    num = 0
+def tournament(p1, p2, number):
     while True:
-        if kim + lim == kim + kim + 1:
-
-    # 참가자 수가 홀수라면 부전승 추가
-    if N // 2 == 1:
-        num += 1
-        match.append(num)
-    # 라운드 수 증가
-    number += 1
-    # 참가자 리스트 교체
-    players = match[:]
-    # 참가자 수 변경
-    N = len(match)
-    # 다음 라운드 진행
-    tournament(N, players, kim, lim, number)
+        # 둘이 붙었을 때 종료
+        if p1 + p2 == p1 + (p1 + 1) or p2 + p1 == p2 + (p2 + 1):
+            # p1 이 짝수가 아니라면 라운드 수 출력
+            if p1 % 2 != 0:
+                print(number)
+                return
+            # p1 이 짝수라면 다음 라운드로 올리기
+            else:
+                p1 = (p1 + 1) // 2
+                p2 = (p2 + 1) // 2
+                number += 1
+        # 둘이 붙지 않으면
+        else:
+            p1 = (p1 + 1) // 2
+            p2 = (p2 + 1) // 2
+            number += 1
 
 
 # 참가자 수 N, 김지민 번호, 임한수 번호
 N, kim, lim = map(int, input().split())
-# 참가자 리스트
-players = [i for i in range(1, N + 1)]
+# 작은 번호를 p1, 큰 번호를 p2 로 지정
+if kim < lim:
+    p1 = kim
+    p2 = lim
+else:
+    p1 = lim
+    p2 = kim
 # 라운드 수
 number = 1
 
-tournament(N, players, kim, lim, number)
+tournament(p1, p2, number)
