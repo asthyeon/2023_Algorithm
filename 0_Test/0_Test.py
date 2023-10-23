557e3226133a762c73bdd627dfd02697b739bc02
asthyeon <asthyeon@naver.com>
2023-10-23 17:52:41 +0900

    1023

--- 0_Test/0_Test.py
+++ 0_Test/0_Test.py
@@ -3,27 +3,44 @@ sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
 """
-# 전공평점 계산기 만들기
-1. 등급이 p인 과목은 계산에서 제외
-2. 20줄이 주어짐
+# 가장 큰 정사각형 구하기
+@ 풀이
+(1) bfs로 풀기
 """
 
-# 각 등급별 점수
-score = {'A+': 4.5, 'A0': 4.0, 'B+': 3.5, 'B0': 3.0,
-         'C+': 2.5, 'C0': 2.0, 'D+': 1.5, 'D0': 1.0, 'F': 0.0}
-# 과목 수
-cnt = 0
-# 총 점수
-total = 0
-# 총 이수학점
-total_credit = 0
-# 입력받기
-for _ in range(20):
-    subject, credit, grade = map(str, input().split())
 
-    # 과목 평점이 P가 아닐 때
-    if grade != 'P':
-       total_credit += float(credit)
-       total += float(credit) * score[grade]
+# bfs 함수
+def bfs(sx, sy):
+    global area
+    global x
+    global y
+    while True:
+        for i in range(x):
+            for j in range(y):
+                if sx + i > n - 1 or sy + j > n - 1:
+                    return
+                if arr[sx + i][sy + j] != 1:
+                    return
+        else:
+            now_area = x * y
+            if area < now_area:
+                area = now_area
+            x += 1
+            y += 1
 
-print(total / total_credit)
+
+# 세로길이 n, 가로길이 m
+n, m = map(int, input().split())
+# 행렬
+arr = [list(map(int, input().rstrip())) for _ in range(n)]
+
+# 최대 넓이
+area = 0
+x = y = 1
+
+for sx in range(n):
+    for sy in range(m):
+        if arr[sx][sy] == 1:
+            bfs(sx, sy)
+
+print(area)
