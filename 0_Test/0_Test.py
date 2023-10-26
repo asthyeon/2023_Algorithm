0c90586fd0bf3bec353e59824283605a1298a133
asthyeon <asthyeon@naver.com>
2023-10-26 17:13:33 +0900

    1026

--- 0_Test/0_Test.py
+++ 0_Test/0_Test.py
@@ -3,44 +3,54 @@ sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
 """
-# 정수 n이 주어졌을 때 1, 2, 3의 합으로 나타내는 방법의 수를 구하는 프로그램
-1. 합을 나타낼 때는 수를 1개 이상 사용하기
-2. 합을 이루고 있는 수의 순서만 다른 것은 같은 것으로 취급
+# n이 주어졌을 때 피보나치 수 F(n) 구하기 (n은 음수 가능)
+1. 피보나치 수의 정의
+ - F(0) = 0(if n = 0;)
+ - F(1) = 1(if n = 1;)
+ - F(n) = F(n - 1) + F(n - 2)(if n > 1;)
+2. 피보나치 수를 n이 음수인 경우로도 확장하기
+ - 위의 식에서 n > 1인 경우에는 성립하는 경우를,
+ - if n <= 1 일 때도 성립되도록 정의하기
+3. 첫째 줄은 양수는 1, 0이면 0, 음수면 -1 출력
+4. 둘째 줄은 F(n)의 (절댓값 / 1,000,000,000) 출력
 @ 풀이
 (1) dp 사용
- - n 을 2로 나눈 수(n // 2) + 1로만 이루어진 수(1개) 값을 더하기
- - n 에서 3 빼기
- - 반복 
 """
 
 
 # dp 함수
 def dynamic_programming(n):
-    # 방법의 수
-    cnt = 0
-    while True:
-        # 3일 때 경우의 수 3개
-        if n == 3:
-            cnt += 3
-            break
-        # 2일 때 경우의 수 2개
-        elif n == 2:
-            cnt += 2
-            break
-        # 1일 때 경우의 수 1개
-        elif n == 1:
-            cnt += 1
-            break
-        cnt += 1
-        cnt += n // 2
-        n -= 3
-
-    return cnt
+    # 절대값 n
+    new_n = abs(n)
+    dp = [0] * 1000001
+    dp[1] = 1
+    dp[2] = 1
+    for i in range(3, new_n + 1):
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000000
+    if new_n == 0:
+        return print(0)
+    elif new_n == 1:
+        return print(1)
+    else:
+        return print(dp[new_n] % 1000000000)
 
 
-T = int(input())
-for tc in range(1, T + 1):
-    # 정수 n
-    n = int(input())
+# 숫자 n
+n = int(input())
 
-    print(dynamic_programming(n))
+# 양수일 때는 1 출력
+if n > 0:
+    print(1)
+# 0일 때는 0 출력
+elif n == 0:
+    print(0)
+# 음수일 때는
+else:
+    # 홀수일 때는 양수이므로 1 출력
+    if abs(n) % 2 == 1:
+        print(1)
+    # 짝수일 때는 음수이므로 -1 출력
+    else:
+        print(-1)
+
+dynamic_programming(n)
