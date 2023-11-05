c542037a1ad078191dd4242d585ce5dd10a83177
asthyeon <asthyeon@gmail.com>
2023-11-06 01:55:59 +0900

    1105

--- 0_Test/0_Test.py
+++ 0_Test/0_Test.py
@@ -3,71 +3,63 @@ sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
 """
-# 최단 경로 길이 출력
-1. 무방향 그래프
-2. 1번에서 N번으로 최단 거리 이동
-3. 임의로 주어진 두 정점은 반드시 통과해야 함
-4. 이동했던 정점, 간선 다시 이동 가능
-5. 최단 경로 없을시 -1 출력
+# 동생을 찾는 가장 빠른 시간 찾기
+1. 수빈이는 N, 동생은 K에 존재
+2. 수빈이는 걷거나 순간이동 가능
+ - 걷기: 1초후에 x-1 or x+1
+ - 순간이동: 0초 후에 2 * X 위치로 이동
 @ 풀이
 (1) 다익스트라 이용
-(2) 1 에서 시작할 때, v1 에서 시작할 때, v2 에서 시작할 때 구하고
-(3) 1 - v1 - v2 - N 경우, 1 - v2 - v1 - N 경우 구하기
 """
 import heapq
 
 
 # 다익스트라 함수
-def dijkstra(adj_list, start):
-    # 거리 리스트
-    distances = [10e9] * (N + 1)
-    # 우선순위 큐
+def dijkstra(N, K):
     pq = []
-    # 시작점 거리, 시작위치
-    heapq.heappush(pq, (0, start))
-    # 시작위치 거리 기록
-    distances[start] = 0
-    # 큐가 빌 때까지
+    # 시간 리스트
+    times = [10e9] * 100001
+    # 시작거리 및 시작점 인큐
+    heapq.heappush(pq, (0, N))
     while pq:
-        dist, now = heapq.heappop(pq)
-        if distances[now] < dist:
+        time, now = heapq.heappop(pq)
+
+        # 동생을 찾았다면 종료
+        if now == K:
+            print(time)
+            exit()
+
+        if times[now] != 10e9:
             continue
 
-        for new_dist, new in adj_list[now]:
-            if distances[new] < dist + new_dist:
-                continue
-            distances[new] = dist + new_dist
-            heapq.heappush(pq, (dist + new_dist, new))
+        # 방문 처리
+        times[now] = time
 
-    return distances
+        # 현재 위치가 동생을 지나쳤다면 좌측 이동만 하기
+        if now > K:
+            # 좌측 이동
+            if now - 1 >= 0:
+                    heapq.heappush(pq, (time + 1, now - 1))
+                    continue
+
+        # 좌측 이동
+        if now - 1 >= 0:
+            if times[now - 1] >= time + 1:
+                heapq.heappush(pq, (time + 1, now - 1))
+
+        # 우측 이동
+        if now + 1 < 100001:
+            if times[now + 1] >= time + 1:
+                heapq.heappush(pq, (time + 1, now + 1))
+
+        # 순간이동
+        if now * 2 < 100001:
+            if times[now * 2] >= time:
+                heapq.heappush(pq, (time, now * 2))
 
 
-# 정점 수 N, 간선 수 E
-N, E = map(int, input().split())
-# 인접 리스트
-adj_list = [[] for _ in range(N + 1)]
-for _ in range(E):
-    # 정점 a, b, 거리 c
-    a, b, c = map(int, input().split())
-    adj_list[a].append((c, b))
-    adj_list[b].append((c, a))
-# 반드시 거쳐야 하는 정점 v1, v2
-v1, v2 = map(int, input().split())
+# 수빈이 위치 N, 동생 위치 K
+N, K = map(int, input().split())
 
-# 1 에서 시작할 떄
-start_one = dijkstra(adj_list, 1)
-# v1 에서 시작할 때
-start_v1 = dijkstra(adj_list, v1)
-# v2 에서 시작할 때
-start_v2 = dijkstra(adj_list, v2)
-# v1 - v2 로 목적지를 갈 때
-v1_first = start_one[v1] + start_v1[v2] + start_v2[N]
-# v2 - v1 로 목적지를 갈 때
-v2_first = start_one[v2] + start_v2[v1] + start_v1[N]
-# 최소값 구하기
-result = min(v1_first, v2_first)
-if result < 10e9:
-    print(result)
-else:
-    print(-1)
+dijkstra(N, K)
 
