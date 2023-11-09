76bc2577e023ccb459ac6de51fb2dc4d2e196dbd
asthyeon <asthyeon@gmail.com>
2023-11-10 00:33:08 +0900

    1109

--- 0_Test/0_Test.py
+++ 0_Test/0_Test.py
@@ -3,80 +3,69 @@ sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
 """
-# 모든 물웅덩이를 덮기 위한 널빤지들의 최소 개수 구하기
-1. 입력으로 주어지는 웅덩이는 겹치지 않음
+# 문자열마다 경우의 수 출력
+1. M x N 행렬은 환형
+2. 상하좌우대각선 이동 가능, 지나온 칸 다시 방문 가능
+3. 방문 순서가 다르면 다른 경우
+4. 중복된 문자열이 들어올 수 있음
 @ 풀이
-(1) 한번 해보기
-위치 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7
-웅덩 0 0 0 0 0     0 0 0 0   0 0 0 0 
-널빤 - - - - - -   - - - - - - - - -
+(1) 재귀를 이용하여 모든 경우의 수 탐색
+(2) 중복은 딕셔너리에 넣어서 처리
 """
+# 8방향 탐색을 위한 튜플
+ds = [(0, 1), (1, 0), (0, -1), (-1, 0), (-1, 1), (1, 1), (1, -1), (-1, -1)]
+# 중복 문자열 방지
+duple = {}
 
-# 웅덩이 수 N, 널빤지의 길이 L
-N, L = map(int, input().split())
-# 웅덩이의 정보
-pools = []
-for _ in range(N):
-    # 웅덩이 위치(s ~ e)
-    s, e = map(int, input().split())
-    pools.append((s, e))
 
-# 널빤지 설치하기
-# 정렬
-pools.sort()
-# 널빤지 수
-plank = 0
-# 웅덩이 위치
-idx = 0
-# 널빤지가 설치된 마지막 위치
-install = -1
-while idx < N:
-    # print(f'# {idx}')
-    # 설치해야 하는 길이
-    length = pools[idx][1] - pools[idx][0]
-    # print(f'original_length: {length}')
+# 탐색(시작좌표, 단어를 찾은 길이, 배열)
+def searching(sx, sy, find, arr):
+    global cnt
+    # 다 찾았다면 경우의 수 +1
+    if find == len(word):
+        cnt += 1
+        return
+    # 8방향 탐색
+    for dx, dy in ds:
+        nx, ny = sx + dx, sy + dy
+        # 벽 형성
+        if 0 <= nx < N and 0 <= ny < M:
+            # 해당 단어가 이번에 찾을 단어와 일치한다면 재귀
+            if arr[nx][ny] == word[find]:
+                searching(nx, ny, find + 1, arr)
+        # 벽을 넘어갈 시
+        else:
+            if nx < 0:
+                nx = N + nx
+            elif nx >= N:
+                nx = nx - N
+            if ny < 0:
+                ny = M + ny
+            elif ny >= M:
+                ny = ny - M
+            # 해당 단어가 이번에 찾을 단어와 일치한다면 재귀
+            if arr[nx][ny] == word[find]:
+                searching(nx, ny, find + 1, arr)
 
-    # 웅덩이의 시작 위치가 현재 널빤지가 설치된 길이보다 이전이라면
-    if pools[idx][0] <= install:
-        # 설치해야 하는 길이 갱신
-        length = pools[idx][1] - install - 1
 
-    # 설치해야 하는 길이가 음수거나 0이라면 넘기기(설치해야 할 필요 X)
-    if length <= 0:
-        idx += 1
-        # print(f'length: {length}')
-        # print(f'pools_now: {pools[idx - 1]}')
-        # print(f'pools: {pools}')
-        # print(f'install: {install}')
-        # print(f'plank: {plank}')
-        continue
-
-    # 웅덩이 길이가 딱 나뉘어질 때
-    if length % L == 0:
-        # 나눈 몫만큼 널빤지 수 더하기
-        plank += length // L
-        # 널빤지가 설치된 마지막 위치 갱신
-        install = pools[idx][1] - 1
-        # 다음 웅덩이로 넘어가기
-        idx += 1
-        # print(f'length: {length}')
-        # print(f'pools_now: {pools[idx - 1]}')
-        # print(f'pools: {pools}')
-        # print(f'install: {install}')
-        # print(f'plank: {plank}')
-        continue
+# 세로 N, 가로 M, 문자열 수 K
+N, M, K = map(int, input().split())
+# 격자판
+arr = [input().rstrip() for _ in range(N)]
+# 문자열 정보
+for _ in range(K):
+    word = input().rstrip()
+    # 경우의 수
+    cnt = 0
+    # 중복이 아니라면 탐색
+    if word not in duple:
+        # 탐색
+        for sx in range(N):
+            for sy in range(M):
+                if arr[sx][sy] == word[0]:
+                    searching(sx, sy, 1, arr)
+        duple[word] = cnt
+        print(cnt)
+    # 중복이라면
     else:
-        # 나눈 몫 + 1만큼 널빤지 수 더하기
-        plank += (length // L) + 1
-        # 널빤지가 설치된 마지막 위치 갱신
-        install = max(pools[idx][0] - 1, install) + (L * ((length // L) + 1))
-        # 다음 웅덩이로 넘어가기
-        idx += 1
-        # print(f'length: {length}')
-        # print(f'pools_now: {pools[idx - 1]}')
-        # print(f'pools: {pools}')
-        # print(f'install: {install}')
-        # print(f'plank: {plank}')
-        continue
-
-print(plank)
+        print(duple[word])
