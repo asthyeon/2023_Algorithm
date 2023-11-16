943c1381d2178c690bab038e24951087944c24b6
asthyeon <asthyeon@naver.com>
2023-11-16 10:49:44 +0900

    1116

--- 0_Test/0_Test.py
+++ 0_Test/0_Test.py
@@ -3,78 +3,92 @@ sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
 """
-# MooTube (Silver)
-[출력: Q개의 줄에 i번째 질문에 대한 K 값에 대한 추천 동영상의 개수를 대답하기]
-1. 두 동영상이 얼마나 가까운 지를 측정하는 단위 "USADO"
-2. N-1개의 동영상 쌍을 골라서 어떤 동영상에서 다른 동영상으로 가는 경로가 반드시 하나 존재
-3. 동영상들을 네트워크 구조로 바꾸고 정점으로 나타냄
-4. 값 K를 정해서 USADO가 K 이상인 모든 동영상이 추천되도록 할 것
-5. 양방향 간선
+# 전구와 스위치
+[출력: 답 출력, 불가능시 -1 출력]
+1. 스위치를 누르면 상태가 반대로 됨(i번 스위치 -> i-1, i, i+1 상태 체인지)
+2. 1번 스위치: 1, 2번 체인지
+   N번 스위치: N-1, N번 체인지
+3. 우리가 만들고자 하는 상태가 주어질 때 최소 몇 번 누르면 되는가
 @ 풀이
-(1) 경로가 반드시 하나 존재 -> MST 구성
-(2) 프림중독자의 프림 구현
+(1) 덱을 사용해 하나씩 체크하면서 바꾸기
+(2) 딕셔너리를 이용해 중복 체크하기
+=> 메모리 초과
+
+(1) 3개씩만 동일하므로 그 부분이 목표와 동일하다면 넘어가는 방식으로 진행하기
+ - 2번 스위치부터 시작해서 이전 번호가 다르다면 누르는 방식으로 진행하기
+ - 1번 스위치를 누른 경우와 2번 스위치를 누른 경우 비교 
+(2) 메모리 초과를 방지하기 위해 덱 사용 X
+(3) 1번 스위치 온오프를 미리 바꿔서 함수를 2번 써보기
 """
-import heapq
 
 
-# 프림 함수
-def prim(start, adj_list):
-    # 우선순위 큐 생성
-    pq = []
-    # 시작거리 및 시작점 인큐
-    heapq.heappush(pq, (0, start))
-    # 방문 리스트
-    MST = [0] * (N + 1)
-    # 추천 동영상 수
-    videos = 0
+# 체크 함수
+def switch(now, target):
+    # 스위치 누른 횟수
+    cnt = 0
+    # 스위치 누르기
+    pushed = now[:]
+    for i in range(1, N):
+        # 이전 전구 상태가 목표 전구 상태와 다르다면 누르기
+        if pushed[i - 1] != target[i - 1]:
+            # 스위치 횟수 추가
+            cnt += 1
+            if pushed[i - 1] == 1:
+                pushed[i - 1] = 0
+            else:
+                pushed[i - 1] = 1
+            if pushed[i] == 1:
+                pushed[i] = 0
+            else:
+                pushed[i] = 1
+            # N번 스위치가 아닐 때는 다음 번호까지 바꾸기
+            if i != N - 1:
+                if pushed[i + 1] == 1:
+                    pushed[i + 1] = 0
+                else:
+                    pushed[i + 1] = 1
 
-    while pq:
-        dist, now = heapq.heappop(pq)
+        # 목표와 일치하면 종료
+        if pushed == target:
+            return cnt
 
-        # 방문 여부 체크
-        if MST[now]:
-            continue
-
-        MST[now] = 1
-
-        # 다음 위치 탐색
-        for new_dist, new in adj_list[now]:
-            # 방문 여부 체크
-            if MST[new]:
-                continue
-
-            # USADO가 K 이상이라면
-            if new_dist >= k:
-                # 방문하지 않은 곳인큐
-                heapq.heappush(pq, (new_dist, new))
-                videos += 1
-
-    return videos
-
-
-# 동영상 수 N, 농부의 질문 수 Q
-N, Q = map(int, input().split())
-# 간선 연결
-adj_list = [[] for _ in range(N + 1)]
-for _ in range(N - 1):
-    # p 와 q 가 r 값으로 이어져 있음
-    p, q, r = map(int, input().split())
-    
-    adj_list[p].append((r, q))
-    adj_list[q].append((r, p))
+    # 불가능시
+    return -1
 
 
-# 농부의 질문 Q개
-for _ in range(Q):
-    # USADO가 k 일 때 v 동영상을 보고 있는 소들에게 몇개의 동영상이 추천되는지
-    k, v = map(int, input().split())
+# 전구 수 N
+N = int(input())
+# 전구의 현재 상태
+now = list(map(int, input().rstrip()))
+# 만들고자 하는 전구의 상태
+target = list(map(int, input().rstrip()))
 
-    print(prim(v, adj_list))
-
-
-
-
-
+# 현재 상태가 목표라면 바로 종료
+if now == target:
+    print(0)
+# 현재 상태가 목표가 아니라면
+else:
+    # 1번 스위치를 안눌렀을 때
+    off = switch(now, target)
+    # 불가능하지 않으면 출력
+    if off != -1:
+        print(off)
+    # 1번 스위치 누르기
+    else:
+        if now[0] == 0:
+            now[0] = 1
+        else:
+            now[0] = 0
+        if now[1] == 0:
+            now[1] = 1
+        else:
+            now[1] = 0
+        on = switch(now, target)
+        # 불가능 여부 체크 후 출력
+        if on != -1:
+            print(on + 1)
+        else:
+            print(on)
 
 
 
