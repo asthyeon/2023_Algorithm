ec28d0334adeb6bc6bfeb2bc95d0104803cfb0d6
asthyeon <asthyeon@naver.com>
2023-11-17 09:32:29 +0900

    1117

--- 0_Test/0_Test.py
+++ 0_Test/0_Test.py
@@ -3,94 +3,62 @@ sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
 """
-# 전구와 스위치
-[출력: 답 출력, 불가능시 -1 출력]
-1. 스위치를 누르면 상태가 반대로 됨(i번 스위치 -> i-1, i, i+1 상태 체인지)
-2. 1번 스위치: 1, 2번 체인지
-   N번 스위치: N-1, N번 체인지
-3. 우리가 만들고자 하는 상태가 주어질 때 최소 몇 번 누르면 되는가
+# 기적의 매매법
+[출력: 준현이 자산이 더 크면 "BNP", 반대는 "TIMING", 같다면 "SAMESAME"]
+1. 준현이: 절대 팔지 않음 
+ - 주식을 살 수 있다면 가능한 만큼 즉시 매수
+2. 성민이: 33매매법
+ - 모든 거래는 전량 매수 전량 매도
+ - 3일 연속 상승주는 다음날 무조건 하락: 3일째 상승 = 전량 매도(반대도 성립)
+3. 내기기간 1일 ~ 14일
+4. 준현이와 성민이 중 누가 더 높은 수익률?
 @ 풀이
-(1) 덱을 사용해 하나씩 체크하면서 바꾸기
-(2) 딕셔너리를 이용해 중복 체크하기
-=> 메모리 초과
-
-(1) 3개씩만 동일하므로 그 부분이 목표와 동일하다면 넘어가는 방식으로 진행하기
- - 2번 스위치부터 시작해서 이전 번호가 다르다면 누르는 방식으로 진행하기
- - 1번 스위치를 누른 경우와 2번 스위치를 누른 경우 비교 
-(2) 메모리 초과를 방지하기 위해 덱 사용 X
-(3) 1번 스위치 온오프를 미리 바꿔서 함수를 2번 써보기
+(1) 구현하기
 """
 
+# 준현이와 성민이에게 주어질 현금
+cash = int(input())
+# 각 일자의 주가
+stocks = list(map(int, input().split()))
 
-# 체크 함수
-def switch(now, target):
-    # 스위치 누른 횟수
-    cnt = 0
-    # 스위치 누르기
-    pushed = now[:]
-    for i in range(1, N):
-        # 이전 전구 상태가 목표 전구 상태와 다르다면 누르기
-        if pushed[i - 1] != target[i - 1]:
-            # 스위치 횟수 추가
-            cnt += 1
-            if pushed[i - 1] == 1:
-                pushed[i - 1] = 0
-            else:
-                pushed[i - 1] = 1
-            if pushed[i] == 1:
-                pushed[i] = 0
-            else:
-                pushed[i] = 1
-            # N번 스위치가 아닐 때는 다음 번호까지 바꾸기
-            if i != N - 1:
-                if pushed[i + 1] == 1:
-                    pushed[i + 1] = 0
-                else:
-                    pushed[i + 1] = 1
+# 준현이 주식수, 현금
+bnp_shares = 0
+bnp_cash = cash
+# 성민이 주식수, 현금
+three_shares = 0
+three_cash = cash
 
-        # 목표와 일치하면 종료
-        if pushed == target:
-            return cnt
+# 주식투자
+for i in range(14):
 
-    # 불가능시
-    return -1
+    # 준현이는 항상 전량 매수
+    bnp_shares += bnp_cash // stocks[i]
+    bnp_cash = bnp_cash % stocks[i]
+    
+    # 성민이는 33매매법
+    # 상승 3일째라면 전량 매도
+    if stocks[i - 2] < stocks[i - 1] < stocks[i]:
+        three_cash += three_shares * stocks[i]
+        three_shares = 0
+    # 하락 3일째~라면 전량 매수
+    elif stocks[i - 2] > stocks[i - 1] > stocks[i]:
+        three_shares += three_cash // stocks[i]
+        three_cash = three_cash % stocks[i]
 
+    print(f'#{i + 1}')
+    print(f'shares: {three_shares}')
+    print(f'cash: {three_cash}')
 
-# 전구 수 N
-N = int(input())
-# 전구의 현재 상태
-now = list(map(int, input().rstrip()))
-# 만들고자 하는 전구의 상태
-target = list(map(int, input().rstrip()))
-
-# 현재 상태가 목표라면 바로 종료
-if now == target:
-    print(0)
-# 현재 상태가 목표가 아니라면
+# 14일 후 자산 비교(남은 현금 + 주식 자산)
+bnp = bnp_cash + (bnp_shares * stocks[-1])
+three = three_cash + (three_shares * stocks[-1])
+print(bnp, three)
+if bnp > three:
+    print('BNP')
+elif bnp < three:
+    print('TIMING')
 else:
-    # 1번 스위치를 안눌렀을 때
-    off = switch(now, target)
-    # 불가능하지 않으면 출력
-    if off != -1:
-        print(off)
-    # 1번 스위치 누르기
-    else:
-        if now[0] == 0:
-            now[0] = 1
-        else:
-            now[0] = 0
-        if now[1] == 0:
-            now[1] = 1
-        else:
-            now[1] = 0
-        on = switch(now, target)
-        # 불가능 여부 체크 후 출력
-        if on != -1:
-            print(on + 1)
-        else:
-            print(on)
-
-
+    print('SAMESAME')
 
 
 
