48235975ae91479eef814c4e9cce59eb537aaedb
asthyeon <asthyeon@gmail.com>
2023-11-19 00:04:35 +0900

    1118

--- 0_Test/0_Test.py
+++ 0_Test/0_Test.py
@@ -3,63 +3,52 @@ sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
 """
-# 기적의 매매법
-[출력: 준현이 자산이 더 크면 "BNP", 반대는 "TIMING", 같다면 "SAMESAME"]
-1. 준현이: 절대 팔지 않음 
- - 주식을 살 수 있다면 가능한 만큼 즉시 매수
-2. 성민이: 33매매법
- - 모든 거래는 전량 매수 전량 매도
- - 3일 연속 상승주는 다음날 무조건 하락: 3일째 상승 = 전량 매도(반대도 성립)
-3. 내기기간 1일 ~ 14일
-4. 준현이와 성민이 중 누가 더 높은 수익률?
+# Yonsei TOTO
+[출력: 주어진 마일리지로 최대로 들을 수 있는 과목 개수 출력]
+1. 듣고 싶은 과목에 마일리지 1~36 분배 가능
+2. 마일리지를 많이 투자한 순으로 그 과목의 수강인원만큼 신청됨
+3. 마일리지가 같으면 성준이에게 우선순위가 주어짐
 @ 풀이
-(1) 구현하기
+(1) 각 과목별로 최소로 들일 수 있는 마일리지 계산하기
+(2) 주어진 마일리지로 최소값부터 분배
 """
 
-# 준현이와 성민이에게 주어질 현금
-cash = int(input())
-# 각 일자의 주가
-stocks = list(map(int, input().split()))
-
-# 준현이 주식수, 현금
-bnp_shares = 0
-bnp_cash = cash
-# 성민이 주식수, 현금
-three_shares = 0
-three_cash = cash
-
-# 주식투자
-for i in range(14):
-
-    # 준현이는 항상 전량 매수
-    bnp_shares += bnp_cash // stocks[i]
-    bnp_cash = bnp_cash % stocks[i]
+# 과목수 n, 주어진 마일리지 m
+n, m = map(int, input().split())
+# 각과목별 필요한 마일리지
+needs = []
+# 과목마다 주어지는 정보
+for _ in range(n):
+    # 과목 신청한 사람 수 P, 수강인원 L
+    P, L = map(int, input().split())
+    # 각 사람이 마일리지를 얼마나 넣었는지 주어짐
+    mileages = list(map(int, input().split()))
     
-    # 성민이는 33매매법
-    # 상승 3일째라면 전량 매도
-    if stocks[i - 2] < stocks[i - 1] < stocks[i]:
-        three_cash += three_shares * stocks[i]
-        three_shares = 0
-    # 하락 3일째~라면 전량 매수
-    elif stocks[i - 2] > stocks[i - 1] > stocks[i]:
-        three_shares += three_cash // stocks[i]
-        three_cash = three_cash % stocks[i]
+    # 마일리지 역순 정렬
+    mileages.sort(reverse=True)
+    
+    # 성준이가 넣어야할 마일리지
+    # 신청한 사람이 수강인원보다 많거나 같다면
+    if P >= L:
+        need = mileages[L - 1]
+        needs.append(need)
+    # 수강인원이 더적다면 마일리지는 최소값 1
+    else:
+        need = 1
+        needs.append(need)
+# 필요한 마일리지 정렬
+needs.sort()
+# 들을 수 있는 과목 수
+cnt = 0
+# 과목수 계산
+for need in needs:
+    if need > 36:
+        break
+    if m >= need:
+        cnt += 1
+        m -= need
+    else:
+        break
 
-    print(f'#{i + 1}')
-    print(f'shares: {three_shares}')
-    print(f'cash: {three_cash}')
-
-# 14일 후 자산 비교(남은 현금 + 주식 자산)
-bnp = bnp_cash + (bnp_shares * stocks[-1])
-three = three_cash + (three_shares * stocks[-1])
-print(bnp, three)
-if bnp > three:
-    print('BNP')
-elif bnp < three:
-    print('TIMING')
-else:
-    print('SAMESAME')
-
-
-
+print(cnt)
 
