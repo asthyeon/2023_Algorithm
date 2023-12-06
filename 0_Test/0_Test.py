1bcc52667034a73944d3abf4855a581cbebbd6b9
asthyeon <asthyeon@gmail.com>
2023-12-07 00:27:26 +0900

    1206

--- 0_Test/0_Test.py
+++ 0_Test/0_Test.py
@@ -3,93 +3,156 @@ sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
 """
-# 감시
-[출력: 사각지대의 최소크기 출력]
-1. CCTV 별로 방향이 존재함
- - 1번: 오
- - 2번: 오, 왼
- - 3번: 오, 위
- - 4번: 오, 왼, 위
- - 5번: 오, 아, 왼, 위
-2. CCTV는 감시할 수 있는 방향에 있는 칸 전체 감시 가능
-3. CCTV는 벽을 통과할 수 없고 감시할 수 없는 영역은 사각지대
-4. CCTV는 90도 방향 회전 가능, CCTV를 통과할 수 있음
-5. CCTV의 방향을 적절히 정해서, 사각 지대의 최소 크기 구하기
+# 주사위 굴리기
+[출력: 이동할 때마다 주사위의 윗 면에 쓰여있는 수 출력, 바깥 이동시 출력 X]
+1. 지도의 좌표는 (r, c)
+2. 주사위는 지도 위에 윗 면이 1, 동쪽을 바라보는 방향이 3인 상태
+3. 가장 처음에 주사위에는 모든 면에 0이 적혀 있음
+4. 주사위를 굴린 후 바닥칸이 0이면 바닥면에 쓰여 있는 수가 칸에 복사됨
+5. 바닥칸이 0이 아니라면 칸에 쓰여있는 수가 바닥면에 복사됨
+6. 주사위가 이동했을 때 마다 상단에 쓰여있는 값 구하기
+7. 바깥으로 이동시 명령 무시하기
+8. 이동명령 - 동쪽: 1 / 서쪽: 2 / 북쪽: 3 / 남쪽: 4
+9. 주사위를 놓은 칸에 쓰여있는 수는 항상 0
 @ 풀이
-(1) 최적의 경우 X -> 모든 경우의 수 구하여 찾기
-(2) CCTV 수로 백트래킹
+(1) 그냥 풀어보기
 """
-# cctv 방향
-dx = (0, 1, 0, -1)
-dy = (1, 0, -1, 0)
-# cctv 번호에 따른 방향
-cctv_dir = [[],
-            # 1번
-            [[0], [1], [2], [3]],
-            # 2번
-            [[0, 2], [1, 3]],
-            # 3번
-            [[0, 3], [1, 0], [2, 1], [3, 2]],
-            # 4번
-            [[0, 2, 3], [1, 3, 0], [2, 0, 1], [3, 1, 2]],
-            # 5번
-            [[0, 1, 2, 3]]]
+# 지도 세로 N, 가로 M, 주사위 좌표 x, y, 명령의 개수 K
+N, M, x, y, K = map(int, input().split())
+arr = []
+# 지도 정보 입력
+for _ in range(N):
+    info = list(map(int, input().split()))
+    arr.append(info)
 
+# 주사위 정보
+dice = [[0] * 3 for _ in range(4)]
 
-# 백트래킹 함수
-def back_tracking(arr, cctv_xy, cctv):
-    global blind_min
-    # 백트래킹 종료조건(모든 cctv의 사각지대 탐색 완료)
-    if cctv == len(cctv_xy):
-        blind = 0
-        for i in range(N):
-            for j in range(M):
-                if arr[i][j] == 0:
-                    blind += 1
-        # 최소값 비교
-        if blind_min > blind:
-            blind_min = blind
-        return
+# 명령 입력
+commands = list(map(int, input().split()))
+for command in commands:
+    # print(f'명령 {command} 현재 위치 ({x}, {y})')
 
-    # 모든 경우를 확인하기 위한 복사본 만들기
-    arr_copy = [o[:] for o in arr[:]]
-    # 사각지대 탐색(cctv 번호의 방향에 따라 여러번 반복)
-    for dir in cctv_dir[cctv_xy[cctv][2]]:
-        # 각 방향만큼 반복
-        for d in dir:
-            nx,  ny = cctv_xy[cctv][0], cctv_xy[cctv][1]
-            while True:
-                nx, ny = nx + dx[d], ny + dy[d]
-                # 벽형성
-                if 0 <= nx < N and 0 <= ny < M:
-                    # 벽을 만날시 중지
-                    if arr_copy[nx][ny] == 6:
-                        break
-                    # 빈 칸일시 감시
-                    elif arr_copy[nx][ny] == 0:
-                        arr_copy[nx][ny] = '#'
-                else:
-                    break
-        # 다음 cctv를 향해 재귀
-        back_tracking(arr_copy, cctv_xy, cctv + 1)
-        # 복사본 초기화
-        arr_copy = [o[:] for o in arr[:]]
+    # 동쪽 이동
+    if command == 1:
+        # 주사위 좌표 이동
+        y += 1
+        # 벽을 넘어갈 시 취소
+        if y >= M:
+            y -= 1
+            continue
+        # 주사위 굴리기
+        # 이동한 칸 확인 후 값 교체
+        if arr[x][y] == 0:
+            # 지도 교체
+            arr[x][y] = dice[1][2]
+        else:
+            # 동쪽면 교체
+            dice[1][2] = arr[x][y]
+            arr[x][y] = 0
+        # 주사위 전개도 리셋
+        dice_copy = [d[:] for d in dice]
+        # 상단 = 서쪽면
+        dice[1][1] = dice_copy[1][0]
+        # 서쪽면 = 바닥
+        dice[1][0] = dice_copy[3][1]
+        # 동쪽면 = 상단
+        dice[1][2] = dice_copy[1][1]
+        # 바닥 = 동쪽면
+        dice[3][1] = dice_copy[1][2]
+        # 상단면 출력
+        print(dice[1][1])
+        # print(f'#동쪽 {dice}')
 
+    # 서쪽 이동
+    elif command == 2:
+        # 주사위 좌표 이동
+        y -= 1
+        # 벽을 넘어갈 시 취소
+        if y < 0:
+            y += 1
+            continue
+        # 주사위 굴리기
+        # 이동한 칸 확인 후 값 교체
+        if arr[x][y] == 0:
+            # 지도 교체
+            arr[x][y] = dice[1][0]
+        else:
+            # 서쪽면 교체
+            dice[1][0] = arr[x][y]
+            arr[x][y] = 0
+        # 주사위 전개도 리셋
+        dice_copy = [d[:] for d in dice]
+        # 상단 = 동쪽면
+        dice[1][1] = dice_copy[1][2]
+        # 서쪽면 = 상단
+        dice[1][0] = dice_copy[1][1]
+        # 동쪽면 = 바닥
+        dice[1][2] = dice_copy[3][1]
+        # 바닥 = 서쪽면
+        dice[3][1] = dice_copy[1][0]
+        # 상단면 출력
+        print(dice[1][1])
+        # print(f'#서쪽 {dice}')
 
-# 세로 N, 가로 M
-N, M = map(int, input().split())
-# 사무실 정보
-office = [list(map(int, input().split())) for _ in range(N)]
+    # 북쪽 이동
+    elif command == 3:
+        # 주사위 좌표 이동
+        x -= 1
+        # 벽을 넘어갈 시 취소
+        if x < 0:
+            x += 1
+            continue
+        # 주사위 굴리기
+        # 이동한 칸 확인 후 값 교체
+        if arr[x][y] == 0:
+            # 지도 교체
+            arr[x][y] = dice[0][1]
+        else:
+            # 북쪽면 교체
+            dice[0][1] = arr[x][y]
+            arr[x][y] = 0
+        # 주사위 전개도 리셋
+        dice_copy = [d[:] for d in dice]
+        # 상단 = 남쪽면
+        dice[1][1] = dice_copy[2][1]
+        # 북쪽면 = 상단
+        dice[0][1] = dice_copy[1][1]
+        # 남쪽면 = 바닥
+        dice[2][1] = dice_copy[3][1]
+        # 바닥 = 북쪽면
+        dice[3][1] = dice_copy[0][1]
+        # 상단면 출력
+        print(dice[1][1])
+        # print(f'#북쪽 {dice}')
 
-# 모든 CCTV 위치 찾기
-cctv_xy = []
-for x in range(N):
-    for y in range(M):
-        if 1 <= office[x][y] <= 5:
-            cctv_xy.append((x, y, office[x][y]))
-
-# 사각지대 최소값
-blind_min = 10e9
-
-back_tracking(office, cctv_xy, 0)
-print(blind_min)
+    # 남쪽 이동
+    elif command == 4:
+        # 주사위 좌표 이동
+        x += 1
+        # 벽을 넘어갈 시 취소
+        if x >= N:
+            x -= 1
+            continue
+        # 주사위 굴리기
+        # 이동한 칸 확인 후 값 교체
+        if arr[x][y] == 0:
+            # 지도 교체
+            arr[x][y] = dice[2][1]
+        else:
+            # 남쪽면 교체
+            dice[2][1] = arr[x][y]
+            arr[x][y] = 0
+        # 주사위 전개도 리셋
+        dice_copy = [d[:] for d in dice]
+        # 상단 = 북쪽면
+        dice[1][1] = dice_copy[0][1]
+        # 북쪽면 = 바닥
+        dice[0][1] = dice_copy[3][1]
+        # 남쪽면 = 상단
+        dice[2][1] = dice_copy[1][1]
+        # 바닥 = 남쪽면
+        dice[3][1] = dice_copy[2][1]
+        # 상단면 출력
+        print(dice[1][1])
+        # print(f'#남쪽 {dice}')
