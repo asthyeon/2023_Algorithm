488de99e317e1facc4d8f4ab209512372d84608b
asthyeon <asthyeon@gmail.com>
2023-12-08 02:25:44 +0900

    1207

--- 0_Test/0_Test.py
+++ 0_Test/0_Test.py
@@ -3,156 +3,115 @@ sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
 """
-# 주사위 굴리기
-[출력: 이동할 때마다 주사위의 윗 면에 쓰여있는 수 출력, 바깥 이동시 출력 X]
-1. 지도의 좌표는 (r, c)
-2. 주사위는 지도 위에 윗 면이 1, 동쪽을 바라보는 방향이 3인 상태
-3. 가장 처음에 주사위에는 모든 면에 0이 적혀 있음
-4. 주사위를 굴린 후 바닥칸이 0이면 바닥면에 쓰여 있는 수가 칸에 복사됨
-5. 바닥칸이 0이 아니라면 칸에 쓰여있는 수가 바닥면에 복사됨
-6. 주사위가 이동했을 때 마다 상단에 쓰여있는 값 구하기
-7. 바깥으로 이동시 명령 무시하기
-8. 이동명령 - 동쪽: 1 / 서쪽: 2 / 북쪽: 3 / 남쪽: 4
-9. 주사위를 놓은 칸에 쓰여있는 수는 항상 0
+# 이차원 배열과 연산
+[출력: A[r][c]에 들어있는 값이 k가 되기 위한 연산의 최소 시간 출력, 100초가 지나도 안되면 -1]
+1. 3 x 3 배열 A의 인덱스는 1부터 시작 및 1초가 지날 때 마다 연산 적용
+ - R 연산 - 행 >= 열: 모든 행 정렬
+ - C 연산 - 행 < 열: 모든 열 정렬 
+2. 정렬하려면 각각의 수가 몇 번 나왔는지 알아야 함
+3. 수의 등장 횟수가 커지는 순으로, 그런 수가 많으면 수가 커지는 순으로 정렬
+4. 정렬된 결과를 다시 A에 넣기
+ - 수와 등장 횟수를 모두 넣으며 순서는 수가 먼저
+ - 다시 넣을 때 R 연산 적용: 가장 큰 행 기준으로 모든 행의 크기가 변함, C 연산은 반대
+ - 행 또는 열의 크기가 커진 곳에는 0이 채워짐
+ - 수를 정렬할 때 0은 무시해야 함
+ - 크기가 100을 넘어가는 경우 100개를 제외한 나머지는 버림
 @ 풀이
 (1) 그냥 풀어보기
+(2) zip을 사용해서 행을 열로 바꿔서 C 연산
 """
-# 지도 세로 N, 가로 M, 주사위 좌표 x, y, 명령의 개수 K
-N, M, x, y, K = map(int, input().split())
-arr = []
-# 지도 정보 입력
-for _ in range(N):
-    info = list(map(int, input().split()))
-    arr.append(info)
 
-# 주사위 정보
-dice = [[0] * 3 for _ in range(4)]
 
-# 명령 입력
-commands = list(map(int, input().split()))
-for command in commands:
-    # print(f'명령 {command} 현재 위치 ({x}, {y})')
+# R 연산 함수
+def R(A):
+    # 0 제거하기
+    ZA = []
+    for aa in A:
+        naa = []
+        for a in aa:
+            if a != 0:
+                naa.append(a)
+        naa.sort()
+        ZA.append(naa)
+    A = ZA
+    # 중복 숫자를 세며 가장 큰 길이 찾기
+    max_length = 0
+    NA = []
+    for aa in A:
+        duple = {}
+        for a in aa:
+            # 중복이 아니면 1
+            if a not in duple:
+                duple[a] = 1
+            # 중복이면 +1
+            else:
+                duple[a] += 1
+        naa = []
+        # value로 정렬하기(100번만)
+        cnt = 0
+        for key, value in sorted(duple.items(), key=lambda x: x[1]):
+            naa.append(key)
+            naa.append(value)
+            cnt += 2
+            if cnt == 100:
+                break
+        NA.append(naa)
+        # 가장 큰 길이 찾기
+        if max_length < len(naa):
+            max_length = len(naa)
+    # 맥스길이와 비교하며 0을 붙이기
+    for naa in NA:
+        if len(naa) < max_length:
+            for _ in range(max_length - len(naa)):
+                naa.append(0)
+    return NA
 
-    # 동쪽 이동
-    if command == 1:
-        # 주사위 좌표 이동
-        y += 1
-        # 벽을 넘어갈 시 취소
-        if y >= M:
-            y -= 1
-            continue
-        # 주사위 굴리기
-        # 이동한 칸 확인 후 값 교체
-        if arr[x][y] == 0:
-            # 지도 교체
-            arr[x][y] = dice[1][2]
-        else:
-            # 동쪽면 교체
-            dice[1][2] = arr[x][y]
-            arr[x][y] = 0
-        # 주사위 전개도 리셋
-        dice_copy = [d[:] for d in dice]
-        # 상단 = 서쪽면
-        dice[1][1] = dice_copy[1][0]
-        # 서쪽면 = 바닥
-        dice[1][0] = dice_copy[3][1]
-        # 동쪽면 = 상단
-        dice[1][2] = dice_copy[1][1]
-        # 바닥 = 동쪽면
-        dice[3][1] = dice_copy[1][2]
-        # 상단면 출력
-        print(dice[1][1])
-        # print(f'#동쪽 {dice}')
 
-    # 서쪽 이동
-    elif command == 2:
-        # 주사위 좌표 이동
-        y -= 1
-        # 벽을 넘어갈 시 취소
-        if y < 0:
-            y += 1
-            continue
-        # 주사위 굴리기
-        # 이동한 칸 확인 후 값 교체
-        if arr[x][y] == 0:
-            # 지도 교체
-            arr[x][y] = dice[1][0]
-        else:
-            # 서쪽면 교체
-            dice[1][0] = arr[x][y]
-            arr[x][y] = 0
-        # 주사위 전개도 리셋
-        dice_copy = [d[:] for d in dice]
-        # 상단 = 동쪽면
-        dice[1][1] = dice_copy[1][2]
-        # 서쪽면 = 상단
-        dice[1][0] = dice_copy[1][1]
-        # 동쪽면 = 바닥
-        dice[1][2] = dice_copy[3][1]
-        # 바닥 = 서쪽면
-        dice[3][1] = dice_copy[1][0]
-        # 상단면 출력
-        print(dice[1][1])
-        # print(f'#서쪽 {dice}')
+# C 연산 함수(행을 열로 바꾸고 R 연산 후 다시 행으로 바꾸기)
+def C(A):
+    # 행을 열로 바꾸기
+    A = [list(r) for r in zip(*A)]
+    # R 연산
+    A = R(A)
+    # 열을 다시 행으로 바꾸기
+    A = [list(c) for c in zip(*A)]
+    
+    return A
 
-    # 북쪽 이동
-    elif command == 3:
-        # 주사위 좌표 이동
-        x -= 1
-        # 벽을 넘어갈 시 취소
-        if x < 0:
-            x += 1
-            continue
-        # 주사위 굴리기
-        # 이동한 칸 확인 후 값 교체
-        if arr[x][y] == 0:
-            # 지도 교체
-            arr[x][y] = dice[0][1]
-        else:
-            # 북쪽면 교체
-            dice[0][1] = arr[x][y]
-            arr[x][y] = 0
-        # 주사위 전개도 리셋
-        dice_copy = [d[:] for d in dice]
-        # 상단 = 남쪽면
-        dice[1][1] = dice_copy[2][1]
-        # 북쪽면 = 상단
-        dice[0][1] = dice_copy[1][1]
-        # 남쪽면 = 바닥
-        dice[2][1] = dice_copy[3][1]
-        # 바닥 = 북쪽면
-        dice[3][1] = dice_copy[0][1]
-        # 상단면 출력
-        print(dice[1][1])
-        # print(f'#북쪽 {dice}')
 
-    # 남쪽 이동
-    elif command == 4:
-        # 주사위 좌표 이동
-        x += 1
-        # 벽을 넘어갈 시 취소
-        if x >= N:
-            x -= 1
-            continue
-        # 주사위 굴리기
-        # 이동한 칸 확인 후 값 교체
-        if arr[x][y] == 0:
-            # 지도 교체
-            arr[x][y] = dice[2][1]
-        else:
-            # 남쪽면 교체
-            dice[2][1] = arr[x][y]
-            arr[x][y] = 0
-        # 주사위 전개도 리셋
-        dice_copy = [d[:] for d in dice]
-        # 상단 = 북쪽면
-        dice[1][1] = dice_copy[0][1]
-        # 북쪽면 = 바닥
-        dice[0][1] = dice_copy[3][1]
-        # 남쪽면 = 상단
-        dice[2][1] = dice_copy[1][1]
-        # 바닥 = 남쪽면
-        dice[3][1] = dice_copy[2][1]
-        # 상단면 출력
-        print(dice[1][1])
-        # print(f'#남쪽 {dice}')
+# 행 r, 열 c, 목표 값 k
+r, c, k = map(int, input().split())
+# 배열 A
+A = [list(map(int, input().split())) for _ in range(3)]
+# 목표값 확인
+# 인덱스에러가 날 경우 넘기기
+if r - 1 >= len(A) or c - 1 >= len(A[0]):
+    pass
+elif A[r - 1][c - 1] == k:
+    print(0)
+    exit()
+
+# 시간
+time = 0
+while time < 100:
+    # 시간 카운트
+    time += 1
+    # 연산 판단하기
+    # print(f'# 행 개수: {len(A)}')
+    # print(f'# 열 개수: {len(A[0])}')
+    # R 연산
+    if len(A) >= len(A[0]):
+        A = R(A)
+    # C 연산
+    else:
+        A = C(A)
+    # 인덱스에러가 날 경우 넘기기
+    if r - 1 >= len(A) or c - 1 >= len(A[0]):
+        continue
+    # 목표값 확인
+    if A[r - 1][c - 1] == k:
+        print(time)
+        exit()
+
+# 시간 오버시
+print(-1)
