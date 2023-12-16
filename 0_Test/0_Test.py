447bb10cf897ce354e73f3e4398db3af5e792c54
asthyeon <asthyeon@gmail.com>
2023-12-16 23:55:02 +0900

    1216

--- 0_Test/0_Test.py
+++ 0_Test/0_Test.py
@@ -3,60 +3,88 @@ sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
 """
-# 최고의 팀 만들기
-[출력: 만들 수 있는 팀 중 가장 큰 능력치를 갖는 팀의 능력치 출력]
-1. 흑 플레이 15명, 백 플레이 15명
-2. 흑, 백의 능력치는 1 ~ 100
-3. 팀 전체 능력치는 흑 전체 능력치 + 백 전체 능력치
-4. 플레이어 한명의 백 능력치, 흑 능력치가 주어짐
-5. 해당 능력치를 토대로 가장 큰 능력치를 갖는 팀 능력치 출력
+# 최솟값과 최댓값
+[출력: 첫째 줄부터 K줄에 걸쳐 구간의 합 출력] 
+1. 어떤 N 개의 수가 주어져 있음(순서대로)
+2. 중간에 수의 변경이 빈번히 일어나고 그 중간에 어떤 부분의 합을 구하려고 함
 @ 풀이
-(1) 그냥 풀어보기
-(2) 우선순위 큐 이용 => 같을 때 처리를 못하겠음
-(3) dp 이용
- - dp[p][w][b] = 현재까지의 능력치들의 합 중 최대값
- - p: 해당 번호 선수
- - w: 백 플레이어일 때
- - b: 흑 플레이어일 때
- 
+(1) 세그먼트 트리 이용
 """
-# 선수정보
-players = []
 
 
-# dp 함수
-def dynamic_programming(players):
-    dp = [[[0] * 16 for _ in range(16)] for _ in range(len(players) + 1)]
-    
-    # 선수 수만큼 반복
-    for p in range(len(players)):
-        # 백 플레이어가 15번이 될 때까지 반복
-        for w in range(16):
-            # 흑 플레이어가 15번이 될 때까지 반복
-            for b in range(16):
-                # 백 플레이어가 15번 전이라면
-                if w < 15:
-                    # 해당 선수를 백 플레이어로 쓰지 않을 때와 쓸 때를 비교
-                    dp[p + 1][w + 1][b] = max(dp[p + 1][w + 1][b], dp[p][w][b] + players[p][0])
-                # 흑 플레이어가 15번 전이라면
-                if b < 15:
-                    # 해당 선수를 백 플레이어로 쓰지 않을 때와 쓸 때를 비교
-                    dp[p + 1][w][b + 1] = max(dp[p + 1][w][b + 1], dp[p][w][b] + players[p][1])
-                # 해당 번호를 그 선수를 쓸 때와 안 쓸 때중 큰 값으로 지정
-                dp[p + 1][w][b] = max(dp[p + 1][w][b], dp[p][w][b])
-
-    return dp[len(players)][-1][-1]
+# 트리 값 입력 함수
+def init(start, end, index):
+    # 가장 끝에 도달하면 값 반환
+    if start == end:
+        tree[index] = integers[start]
+        return tree[index]
+    # 도달하지 않았다면 좌우 나누며 재귀
+    mid = (start + end) // 2
+    left = init(start, mid, index * 2)
+    right = init(mid + 1, end, (index * 2) + 1)
+    # 좌측 노드와 우측 노드를 채우며 부모 노드의 값도 채우기
+    tree[index] = left + right
+    return tree[index]
 
 
-while True:
-    # 입력이 있을 때
-    try:
-        # 백 능력치, 흑 능력치
-        white, black = map(int, input().split())
-        # 선수정보에 넣기
-        players.append((white, black))
+# 구간 합 구하는 함수(first ~ last 까지의 합)
+def prefix_sum(start, end, index, first, last):
+    # 범위 밖에 있는 경우 기본값 반환
+    if first > end or last < start:
+        return 0
+    # 범위 안에 있는 경우 값 반환
+    if first <= start and last >= end:
+        return tree[index]
+    # 그렇지 않다면 두 부분으로 나누어 합 구하기
+    mid = (start + end) // 2
+    left = prefix_sum(start, mid, index * 2, first, last)
+    right = prefix_sum(mid + 1, end, (index * 2) + 1, first, last)
+    return left + right
 
-    # 입력이 없으면 결과 출력 후 반복 종료
-    except:
-        print(dynamic_programming(players))
-        break
+
+# 수를 수정하는 함수
+def modify(start, end, index, target, modified):
+    # 수정할 수가 범위 밖에 있는 경우 끝내기
+    if target < start or target > end:
+        return
+    # 범위 안에 있으면 그 지점까지 갱신
+    tree[index] += modified
+    if start == end:
+        return
+    mid = (start + end) // 2
+    # 왼쪽 자식 및 오른쪽 자식 수정
+    modify(start, mid, index * 2, target, modified)
+    modify(mid + 1, end, (index * 2) + 1, target, modified)
+
+
+# 트리 생성
+tree = [0] * ((1500 ** 2) * 2)
+
+# 수의 개수 N, 변경 횟수 M, 구간의 합 구하는 횟수 K
+N, M, K = map(int, input().split())
+# N개의 수
+integers = []
+for _ in range(N):
+    integer = int(input())
+    integers.append(integer)
+
+# 트리에 값 입력
+init(0, N - 1, 1)
+
+# a가 1인 경우 b번째 수를 c로 바꾸기
+# a가 2인 경우 b번째 수부터 c번째 수까지의 합을 구하여 출력
+for _ in range(M + K):
+    a, b, c = map(int, input().split())
+
+    if a == 1:
+        # c 에서 b만큼 값을 뺀 값으로 바꾸기
+        modified = c - integers[b - 1]
+        # 정수 리스트 수정하기
+        integers[b - 1] = c
+        # 바뀐 수를 이용해 수정
+        modify(0, N - 1, 1, b - 1, modified)
+
+    else:
+        # 구간 합 출력
+        print(prefix_sum(0, N - 1, 1, b - 1, c - 1))
+
--- 0_Test/jj_test.py
+++ 0_Test/jj_test.py
@@ -2,19 +2,55 @@ import sys
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
-info = []
-for line in sys.stdin: info.append([int(e) for e in line.split()])
-sz = len(info)
-dp = [[[0 for _ in range(15 + 5)] for _ in range(15 + 5)] for _ in range(sz + 1)]
+"""
+# 택배 배달과 수거하기
+[출력: ]
+1. n개의 집에 택배 배달
+2. 배달할 물건은 모두 크기가 같음
+3. i번째 집은 창고에서 i만큼 거리가 떨어져 있음
+4. i번째 집은 j번째 집과 거리(j - i)만큼 떨어져 있음
+5. 트럭에는 택배를 cap개 실을 수 있음
+6. 트럭 하나로 모든 배달과 수거를 마치는 최소 거리 파악하기
+7. 각 집에 배달 및 수거할 때 원하는 개수만큼 배달 및 수거 가능
+@ 풀이[1시간 30분 안에 못품]
+(1) 그냥 풀어보기 
+(2) 최대한 뒤에서부터 해결을 미리 해놓아야 뒤에 다시 안옴
+(3) 누적합 이용하여 풀기
+"""
 
-for i in range(sz):
-    for w in range(15 + 1):
-        for b in range(15 + 1):
-            if w + 1 <= 15:
-                dp[i + 1][w + 1][b] = max(dp[i + 1][w + 1][b], dp[i][w][b] + info[i][0])
-            if b + 1 <= 15:
-                dp[i + 1][w][b + 1] = max(dp[i + 1][w][b + 1], dp[i][w][b] + info[i][1])
-            dp[i + 1][w][b] = max(dp[i + 1][w][b], dp[i][w][b])
 
-print(info)
-print(dp[sz][15][15])
+# 트럭 최대 적재 cap, 배달 집 수 n
+# 각 집에 배달할 택배 리스트 deliveries, 회수 리스트 pickups
+def solution(cap, n, deliveries, pickups):
+    print(deliveries)
+    print(pickups)
+    deliveries.sort()
+    pr
+    # 뒤에서부터 반복해야 하므로 재정렬
+    deliveries = deliveries.sort(reverse=True)
+    pickups = pickups.sort(reverse=True)
+    # 최소 거리
+    answer = 0
+    # 현재 배달, 회수해야 하는 택배 수
+    now_d = 0
+    now_p = 0
+
+    print(deliveries)
+    print(pickups)
+
+    # 뒤에서부터 반복
+    for now in range(n):
+        # 이번에 배달해야하는 택배 및 회수 수 더하기
+        now_d += deliveries[now]
+        now_p += pickups[now]
+        # 이번 배달하거나 회수해야할 것이 남아있는지 확인하고 남았다면 반복
+        while now_d > 0 or now_p > 0:
+            # 최대 가능 개수를 통해서 확인하기
+            now_d -= cap
+            now_p -= cap
+            # 반복한 거리만큼 최소 거리에 더하기(왕복이므로 곱하기 2)
+            answer += (n - now) * 2
+
+    return answer
+
+solution(4, 5, [1, 0, 3, 1, 2], [0, 3, 0, 4, 0])
