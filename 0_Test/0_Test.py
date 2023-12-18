3c24a5efa137d15927e8208d5e4e7790a2066cd3
asthyeon <asthyeon@gmail.com>
2023-12-18 12:01:32 +0900

    1218

--- 0_Test/0_Test.py
+++ 0_Test/0_Test.py
@@ -3,10 +3,12 @@ sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
 """
-# 최솟값과 최댓값
-[출력: 첫째 줄부터 K줄에 걸쳐 구간의 합 출력] 
-1. 어떤 N 개의 수가 주어져 있음(순서대로)
-2. 중간에 수의 변경이 빈번히 일어나고 그 중간에 어떤 부분의 합을 구하려고 함
+# 커피숍2
+[출력: 한 턴마다 구한 합을 한 줄마다 한 개씩 출력]
+1. N개의 정수가 있으면 구간합을 구하거나 수를 수정해야하는 경우가 있음
+2. 구간합과 수정을 할 수 있는 기능 필요
+3. x, y는 x ~ y까지의 합(x > y 인 경우 y ~ x)
+4. a, b는 a번째 수를 b로 바꾸기
 @ 풀이
 (1) 세그먼트 트리 이용
 """
@@ -14,77 +16,75 @@ input = sys.stdin.readline
 
 # 트리 값 입력 함수
 def init(start, end, index):
-    # 가장 끝에 도달하면 값 반환
+    # 끝까지 도달하면 값 입력 및 반환
     if start == end:
         tree[index] = integers[start]
         return tree[index]
-    # 도달하지 않았다면 좌우 나누며 재귀
+    # 도달하지 않았다면 좌우 나누며 자식 노드로 재귀
     mid = (start + end) // 2
     left = init(start, mid, index * 2)
     right = init(mid + 1, end, (index * 2) + 1)
-    # 좌측 노드와 우측 노드를 채우며 부모 노드의 값도 채우기
+    # 자식 노드들을 채우며 부모 노드의 값도 채우기
     tree[index] = left + right
     return tree[index]
 
 
-# 구간 합 구하는 함수(first ~ last 까지의 합)
+# 구간 합 구하는 함수(first ~ last)
 def prefix_sum(start, end, index, first, last):
-    # 범위 밖에 있는 경우 기본값 반환
+    # 구간이 범위를 벗어나면 기본값 반환
     if first > end or last < start:
         return 0
-    # 범위 안에 있는 경우 값 반환
+    # 구간이 시작값보다 작거나 같고 마지막값보다 크거나 같으면 구간 값 리턴
     if first <= start and last >= end:
         return tree[index]
-    # 그렇지 않다면 두 부분으로 나누어 합 구하기
+    # 그렇지 않다면 좌우 나누며 자식 노드로 재귀 후 합 구하기
     mid = (start + end) // 2
     left = prefix_sum(start, mid, index * 2, first, last)
     right = prefix_sum(mid + 1, end, (index * 2) + 1, first, last)
     return left + right
 
 
-# 수를 수정하는 함수
+# 값 수정 함수
 def modify(start, end, index, target, modified):
-    # 수정할 수가 범위 밖에 있는 경우 끝내기
+    # 수정할 수가 범위 밖에 있는 경우 종료
     if target < start or target > end:
         return
-    # 범위 안에 있으면 그 지점까지 갱신
+    # 범위 안에 있으면 변경된 값만큼 더하기
     tree[index] += modified
+    # 리프 노드라면 종료
     if start == end:
         return
+    # 자식 노드라면 좌우 나누며 재귀
     mid = (start + end) // 2
-    # 왼쪽 자식 및 오른쪽 자식 수정
     modify(start, mid, index * 2, target, modified)
     modify(mid + 1, end, (index * 2) + 1, target, modified)
 
 
+# 수의 개수 N, 턴의 개수 Q
+N, Q = map(int, input().split())
+# 처음 배열에 들어가 있는 정수 N개
+integers = list(map(int, input().split()))
+
 # 트리 생성
-tree = [0] * ((1500 ** 2) * 2)
-
-# 수의 개수 N, 변경 횟수 M, 구간의 합 구하는 횟수 K
-N, M, K = map(int, input().split())
-# N개의 수
-integers = []
-for _ in range(N):
-    integer = int(input())
-    integers.append(integer)
-
-# 트리에 값 입력
+tree = [0] * (N * 4)
+# 트리 값 입력
 init(0, N - 1, 1)
 
-# a가 1인 경우 b번째 수를 c로 바꾸기
-# a가 2인 경우 b번째 수부터 c번째 수까지의 합을 구하여 출력
-for _ in range(M + K):
-    a, b, c = map(int, input().split())
+# 턴
+for _ in range(Q):
+    # x ~ y의 합을 구하고, a번째 수를 b로 바꾸기
+    x, y, a, b = map(int, input().split())
 
-    if a == 1:
-        # c 에서 b만큼 값을 뺀 값으로 바꾸기
-        modified = c - integers[b - 1]
-        # 정수 리스트 수정하기
-        integers[b - 1] = c
-        # 바뀐 수를 이용해 수정
-        modify(0, N - 1, 1, b - 1, modified)
-
+    # 구간 합 출력(x, y 크기 비교)
+    if x > y:
+        print(prefix_sum(0, N - 1, 1, y - 1, x - 1))
     else:
-        # 구간 합 출력
-        print(prefix_sum(0, N - 1, 1, b - 1, c - 1))
+        print(prefix_sum(0, N - 1, 1, x - 1, y - 1))
 
+    # 값 수정하기
+    # a번째 수에서 b만큼 값을 뺀 값으로 바꾸기
+    modified = b - integers[a - 1]
+    # 정수 리스트 수정하기
+    integers[a - 1] = b
+    # 수정
+    modify(0, N - 1, 1, a - 1, modified)
