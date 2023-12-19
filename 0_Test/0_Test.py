1484211652c711eddfaff3ad1e18f4afe67880c1
asthyeon <asthyeon@gmail.com>
2023-12-19 11:37:03 +0900

    1219

--- 0_Test/0_Test.py
+++ 0_Test/0_Test.py
@@ -3,88 +3,111 @@ sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
 """
-# 커피숍2
-[출력: 한 턴마다 구한 합을 한 줄마다 한 개씩 출력]
-1. N개의 정수가 있으면 구간합을 구하거나 수를 수정해야하는 경우가 있음
-2. 구간합과 수정을 할 수 있는 기능 필요
-3. x, y는 x ~ y까지의 합(x > y 인 경우 y ~ x)
-4. a, b는 a번째 수를 b로 바꾸기
+# 수열과 쿼리 15
+1. 길이가 N인 수열
+2. 다음 쿼리 수행
+ - 1 i v : A[i]를 v로 바꾸기
+ - 2 : 수열에서 크기가 가장 작은 값의 인덱스를 출력
+ - 2의 경우가 여러 개인 경우 인덱스가 작은 것을 출력
+3. 수열의 인덱스는 1부터 시작
 @ 풀이
 (1) 세그먼트 트리 이용
+(2) 인덱스를 구해야 하므로 인덱스 값도 함께 저장해서 구하기
 """
 
 
 # 트리 값 입력 함수
 def init(start, end, index):
-    # 끝까지 도달하면 값 입력 및 반환
+    # 가장 끝까지 도달하면 인덱스와 값 부여
     if start == end:
-        tree[index] = integers[start]
+        tree[index] = [start, A[start]]
         return tree[index]
-    # 도달하지 않았다면 좌우 나누며 자식 노드로 재귀
+    # 도달하지 않았으면 좌우 나누고 자식 노드로 재귀
     mid = (start + end) // 2
     left = init(start, mid, index * 2)
     right = init(mid + 1, end, (index * 2) + 1)
-    # 자식 노드들을 채우며 부모 노드의 값도 채우기
-    tree[index] = left + right
+    # 자식 노드의 값을 채우며 부모 노드의 값도 채우기
+    # 자식 노드들중 더 작은 값을 찾고 부여하기
+    if left[1] > right[1]:
+        tree[index] = [right[0], right[1]]
+    else:
+        tree[index] = [left[0], left[1]]
     return tree[index]
 
 
-# 구간 합 구하는 함수(first ~ last)
-def prefix_sum(start, end, index, first, last):
-    # 구간이 범위를 벗어나면 기본값 반환
+# 트리 최소값 찾는 함수
+def min_value(start, end, index, first, last):
+    # 구간이 범위를 벗어나면 기본 값 반환
     if first > end or last < start:
-        return 0
-    # 구간이 시작값보다 작거나 같고 마지막값보다 크거나 같으면 구간 값 리턴
+        return 10e9
+
+    # 구간이 수열의 크기보다 크다면 구간 값 반환
     if first <= start and last >= end:
         return tree[index]
-    # 그렇지 않다면 좌우 나누며 자식 노드로 재귀 후 합 구하기
+    
+    # 그렇지 않다면 좌우로 나누고 자식 노드로 재귀
     mid = (start + end) // 2
-    left = prefix_sum(start, mid, index * 2, first, last)
-    right = prefix_sum(mid + 1, end, (index * 2) + 1, first, last)
-    return left + right
+    left = min_value(start, mid, index * 2, first, last)
+    right = min_value(mid + 1, end, (index * 2) + 1, first, last)
+    # 자식 노드들중 더 작은 값을 찾고 리턴
+    if left[1] > right[1]:
+        return [right[0], right[1]]
+    else:
+        return [left[0], left[1]]
 
 
-# 값 수정 함수
+# 트리 값 수정 함수
 def modify(start, end, index, target, modified):
-    # 수정할 수가 범위 밖에 있는 경우 종료
+    # 수정할 값이 범위를 벗어나면 종료
     if target < start or target > end:
         return
-    # 범위 안에 있으면 변경된 값만큼 더하기
-    tree[index] += modified
-    # 리프 노드라면 종료
+    # 리프노드라면 값 수정
     if start == end:
+        tree[index] = [start, modified]
         return
-    # 자식 노드라면 좌우 나누며 재귀
+    # 리프 노드가 아니라면 좌우 나누며 자식 노드로 재귀
     mid = (start + end) // 2
     modify(start, mid, index * 2, target, modified)
     modify(mid + 1, end, (index * 2) + 1, target, modified)
 
+    # 더 작은 값으로 부모 노드 값 수정
+    if tree[index * 2][1] > tree[(index * 2) + 1][1]:
+        tree[index] = [tree[(index * 2) + 1][0], tree[(index * 2) + 1][1]]
+    else:
+        tree[index] = [tree[index * 2][0], tree[index * 2][1]]
 
-# 수의 개수 N, 턴의 개수 Q
-N, Q = map(int, input().split())
-# 처음 배열에 들어가 있는 정수 N개
-integers = list(map(int, input().split()))
+
+# 수열의 크기 N
+N = int(input())
+# 수열
+A = list(map(int, input().split()))
+# 쿼리의 개수 M
+M = int(input())
 
 # 트리 생성
-tree = [0] * (N * 4)
+tree = [10e9] * (N * 4)
 # 트리 값 입력
 init(0, N - 1, 1)
 
-# 턴
-for _ in range(Q):
-    # x ~ y의 합을 구하고, a번째 수를 b로 바꾸기
-    x, y, a, b = map(int, input().split())
+# print(f'A: {A}')
+# print(f'tree: {tree[:N + 1]}')
 
-    # 구간 합 출력(x, y 크기 비교)
-    if x > y:
-        print(prefix_sum(0, N - 1, 1, y - 1, x - 1))
+# 쿼리 수행
+for _ in range(M):
+    query = input().rstrip()
+
+    # 쿼리가 2일 때 수행
+    if query == '2':
+        index, value = min_value(0, N - 1, 1, 0, N - 1)
+        print(index + 1)
+
+    # 쿼리가 1일 때 수행
     else:
-        print(prefix_sum(0, N - 1, 1, x - 1, y - 1))
-
-    # 값 수정하기
-    # a번째 수에서 b만큼 값을 뺀 값으로 바꾸기
-    modified = b - integers[a - 1]
-    # 정수 리스트 수정하기
-    integers[a - 1] = b
-    # 수정
-    modify(0, N - 1, 1, a - 1, modified)
+        # 쿼리를 나누기
+        query, i, v = query.split(' ')
+        # 수열값 수정
+        A[int(i) - 1] = int(v)
+        # 수정
+        modify(0, N - 1, 1, int(i) - 1, int(v))
+        # print(f'A: {A}')
+        # print(f'tree: {tree[:N + 1]}')
--- 0_Test/jj_test.py
+++ 0_Test/jj_test.py
@@ -3,54 +3,101 @@ sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
 """
-# 택배 배달과 수거하기
-[출력: ]
-1. n개의 집에 택배 배달
-2. 배달할 물건은 모두 크기가 같음
-3. i번째 집은 창고에서 i만큼 거리가 떨어져 있음
-4. i번째 집은 j번째 집과 거리(j - i)만큼 떨어져 있음
-5. 트럭에는 택배를 cap개 실을 수 있음
-6. 트럭 하나로 모든 배달과 수거를 마치는 최소 거리 파악하기
-7. 각 집에 배달 및 수거할 때 원하는 개수만큼 배달 및 수거 가능
-@ 풀이[1시간 30분 안에 못품]
-(1) 그냥 풀어보기 
-(2) 최대한 뒤에서부터 해결을 미리 해놓아야 뒤에 다시 안옴
-(3) 누적합 이용하여 풀기
+# 최솟값과 최댓값
+[출력: M개의 줄에 입력받은 순서대로 각 a, b에 대한 답을 최솟값, 최댓값 순서로 출력]
+1. a번째 정수부터 b번째 정수 중 제일 작은 정수, 큰 정수 찾기
+2. a, b 쌍이 M개 주어짐
+3. 입력된 순서대로 a ~ b 번 정수 중 최소, 최댓값 찾기
+@ 풀이
+(1) 세그먼트 트리 이용
+ - 여러 개의 데이터가 존재할 때 특정 구간의 결과값을 구하는데 사용
+ - 이진 트리의 형태
+ - 선형 탐색보다 효율적
+ - 시간복잡도: O(logN)
+ - 단점으로는 완탐에 비해 더 많은 메모리가 필요
 """
 
 
-# 트럭 최대 적재 cap, 배달 집 수 n
-# 각 집에 배달할 택배 리스트 deliveries, 회수 리스트 pickups
-def solution(cap, n, deliveries, pickups):
-    print(deliveries)
-    print(pickups)
-    deliveries.sort()
-    pr
-    # 뒤에서부터 반복해야 하므로 재정렬
-    deliveries = deliveries.sort(reverse=True)
-    pickups = pickups.sort(reverse=True)
-    # 최소 거리
-    answer = 0
-    # 현재 배달, 회수해야 하는 택배 수
-    now_d = 0
-    now_p = 0
+# 최댓값 트리 생성 함수
+def max_init(start, end, index):
+    # 가장 끝에 도달했으면 값 배정
+    if start == end:
+        max_tree[index] = integers[start]
+        return max_tree[index]
+    # 도달하지 않았다면 좌우 나누기
+    mid = (start + end) // 2
+    left = max_init(start, mid, index * 2)
+    right = max_init(mid + 1, end, (index * 2) + 1)
+    # 좌측 노드와 우측 노드를 채워주면서 부모 노드의 값도 채우기
+    max_tree[index] = max(left, right)
+    return max_tree[index]
 
-    print(deliveries)
-    print(pickups)
 
-    # 뒤에서부터 반복
-    for now in range(n):
-        # 이번에 배달해야하는 택배 및 회수 수 더하기
-        now_d += deliveries[now]
-        now_p += pickups[now]
-        # 이번 배달하거나 회수해야할 것이 남아있는지 확인하고 남았다면 반복
-        while now_d > 0 or now_p > 0:
-            # 최대 가능 개수를 통해서 확인하기
-            now_d -= cap
-            now_p -= cap
-            # 반복한 거리만큼 최소 거리에 더하기(왕복이므로 곱하기 2)
-            answer += (n - now) * 2
+# 최솟값 트리 생성 함수
+def min_init(start, end, index):
+    # 가장 끝에 도달했으면 값 배정
+    if start == end:
+        min_tree[index] = integers[start]
+        return min_tree[index]
+    # 도달하지 않았다면 좌우 나누기
+    mid = (start + end) // 2
+    left = min_init(start, mid, index * 2)
+    right = min_init(mid + 1, end, (index * 2) + 1)
+    # 좌측 노드와 우측 노드를 채워주면서 부모 노드의 값도 채우기
+    min_tree[index] = min(left, right)
+    return min_tree[index]
 
-    return answer
 
-solution(4, 5, [1, 0, 3, 1, 2], [0, 3, 0, 4, 0])
+# 최댓값 구하는 함수
+def maximum(start, end, index, left, right):
+    # 범위 밖에 있는 경우
+    if left > end or right < start:
+        return 0
+    # 범위 안에 있는 경우
+    if left <= start and right >= end:
+        return max_tree[index]
+    # 그렇지 않다면 두 부분으로 나누기
+    mid = (start + end) // 2
+    # start와 end가 변하면서 최댓값 찾기
+    return max(maximum(start, mid, index * 2, left, right), maximum(mid + 1, end, (index * 2) + 1, left, right))
+
+
+# 최솟값 구하는 함수
+def minimum(start, end, index, left, right):
+    # 범위 밖에 있는 경우
+    if left > end or right < start:
+        return 10e10
+    # 범위 안에 있는 경우
+    if left <= start and right >= end:
+        return min_tree[index]
+    # 그렇지 않다면 두 부분으로 나누기
+    mid = (start + end) // 2
+    # start와 end가 변하면서 최댓값 찾기
+    return min(minimum(start, mid, index * 2, left, right), minimum(mid + 1, end, (index * 2) + 1, left, right))
+
+
+# 정수 개수 N, a, b의 쌍 수 M
+N, M = map(int, input().split())
+# N 개의 정수
+integers = []
+for _ in range(N):
+    integer = int(input())
+    integers.append(integer)
+
+# 트리 생성(배열의 개수가 N개일 때, N보다 큰 가장 가까운 N의 제곱수를 구한 뒤에 그것의 2배)
+# 넉넉하게 만들기
+max_tree = [0] * ((400 ** 2) * 2)
+min_tree = [10e10] * ((400 ** 2) * 2)
+
+# 트리에 값 부여
+max_init(0, N - 1, 1)
+min_init(0, N - 1, 1)
+print(min_tree[:40])
+
+# M 개의 a, b 쌍마다 구하기
+for _ in range(M):
+    a, b = map(int, input().split())
+
+    ans_min = minimum(0, N - 1, 1, a - 1, b - 1)
+    ans_max = maximum(0, N - 1, 1, a - 1, b - 1)
+    print(ans_min, ans_max)
