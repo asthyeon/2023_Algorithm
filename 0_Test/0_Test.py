46adb149ebdc4bcc1bdafbce1d273d4375dcb674
asthyeon <asthyeon@gmail.com>
2024-01-11 17:08:08 +0900

    0111

--- 0_Test/0_Test.py
+++ 0_Test/0_Test.py
@@ -3,62 +3,81 @@ sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
 """
-# 직각삼각형
-1. 2차원 평면에 N개의 점이 주어져 있음
-2. 이 중에서 세 점을 골랐을 때 직각삼각형이 몇 개나 있는지 구하기
+# 텀 프로젝트
+1. 프로젝트 팀을 만들기 위해 모든 학생등를 함께하고 싶은 학생들을 선택해야 함
+ - 프로젝트 팀원 수에는 제한 X(모든 학생들이 동일한 팀의 팀원인 경우 한 팀만 만들어짐)
+ - 한 명당 한 명만 선택 가능
+ - 혼자 하고 싶어하는 학생은 자기 자신을 선택하는 것도 가능
+2. 주어진 선택의 결과를 보고 어느 프로젝트 팀에도 속하지 않는 학생들의 수를 계산하기
 * 입력
-- N: 점의 개수(3 <= N <= 1,500)
-- 둘째 줄부터 N개의 줄에 걸쳐 각 점의 x좌표와 y좌표가 빈 칸을 사이에 두고 주어짐
-  (주어지는 모든 점의 좌표는 다름)
-[출력: 첫째 줄에 직각삼각형의 개수를 출력하기]
+- 첫째 줄 T: 테스트 케이스의 개수
+- 각 테스트의 첫째 줄 n: 학생의 수
+- 각 테스트의 둘째 줄: 선택된 학생들의 번호가 주어짐
+[출력: 각 줄에 프로젝트 팀에 속하지 못한 학생들의 수 나타내기]
 """
 
 """
 @ 풀이
-(1) 조합으로 세 점을 찾는 경우의 수 구하기
-(2) 세 점으로 직각 삼각형이 되는 조건 확인 => 메모리 초과
- - a^2 + b^2 = c^2
-(3) 삼중포문으로 되는 조건 확인
+(1) 예시 이해해보기
+1 -> 3 X
+2 -> 1 X
+3 -> 3 (3)팀
+4 -> 7 (4, 6, 7)팀
+5 -> 3 X
+6 -> 4 (4, 6, 7)팀
+7 -> 6 (4, 6, 7)팀
+(2) dfs로 탐색
 """
 
 
-# 직각삼각형 찾는 함수
-def find(locations, a, b, c):
-    # 각 점의 위치 입력
-    x1, y1 = locations[a][0], locations[a][1]
-    x2, y2 = locations[b][0], locations[b][1]
-    x3, y3 = locations[c][0], locations[c][1]
-
-    # 각 선분 길이 구하기
-    a_square = (x1 - x2) ** 2 + (y1 - y2) ** 2
-    b_square = (x2 - x3) ** 2 + (y2 - y3) ** 2
-    c_square = (x3 - x1) ** 2 + (y3 - y1) ** 2
-
-    # 직각삼각형 확인 후 맞으면 개수 추가
-    if a_square + b_square == c_square:
-        return 1
-    elif b_square + c_square == a_square:
-        return 1
-    elif c_square + a_square == b_square:
-        return 1
-    # 직각삼각형이 아니라면 추가 X
-    else:
+# dfs 함수
+def dfs(start, used):
+    # 팀 프로젝트에 사용된 숫자라면 종료
+    if start in used:
         return 0
+    # 혼자 팀이라면 종료
+    if start == choices[start]:
+        used[start] = 1
+        return 0
+    # 방문 리스트
+    visited = [0] * (n + 1)
+    # 이번 프로젝트 팀
+    project = []
+
+    while True:
+        # 방문한 곳이라면 싸이클이므로(팀 프로젝트 이므로) 종료
+        if visited[start] == 1:
+            for num in project:
+                used[num] = 1
+            return
+        # 방문 기록
+        visited[start] = 1
+        # 이동
+        start = choices[start]
 
 
-# 점의 개수 N
-N = int(input())
-# 각 점의 좌표들
-locations = []
-for _ in range(N):
-    x, y = map(int, input().split())
-    locations.append((x, y))
+# 테스트 케이스 수
+T = int(input())
+for tc in range(1, T + 1):
+    # 학생 수 n
+    n = int(input())
+    # 학생들이 선택한 번호(편의를 위해 0번 추가)
+    choices = [0]
+    choices += list(map(int, input().split()))
 
-# 각 세점마다 직각삼각형 확인
-answer = 0
-for a in range(N - 2):
-    for b in range(a + 1, N - 1):
-        for c in range(b + 1, N):
-            answer += find(locations, a, b, c)
+    # 사용된 숫자인지 확인
+    used = {}
+    # dfs 탐색
+    for start in range(1, n + 1):
+        dfs(start, used)
 
-print(answer)
+    print(used)
+
+
+
+
+
+
+
+
+
--- 0_Test/jj_test.py
+++ 0_Test/jj_test.py
@@ -2,102 +2,44 @@ import sys
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
-"""
-# 최솟값과 최댓값
-[출력: M개의 줄에 입력받은 순서대로 각 a, b에 대한 답을 최솟값, 최댓값 순서로 출력]
-1. a번째 정수부터 b번째 정수 중 제일 작은 정수, 큰 정수 찾기
-2. a, b 쌍이 M개 주어짐
-3. 입력된 순서대로 a ~ b 번 정수 중 최소, 최댓값 찾기
-@ 풀이
-(1) 세그먼트 트리 이용
- - 여러 개의 데이터가 존재할 때 특정 구간의 결과값을 구하는데 사용
- - 이진 트리의 형태
- - 선형 탐색보다 효율적
- - 시간복잡도: O(logN)
- - 단점으로는 완탐에 비해 더 많은 메모리가 필요
-"""
+from collections import deque
 
 
-# 최댓값 트리 생성 함수
-def max_init(start, end, index):
-    # 가장 끝에 도달했으면 값 배정
-    if start == end:
-        max_tree[index] = integers[start]
-        return max_tree[index]
-    # 도달하지 않았다면 좌우 나누기
-    mid = (start + end) // 2
-    left = max_init(start, mid, index * 2)
-    right = max_init(mid + 1, end, (index * 2) + 1)
-    # 좌측 노드와 우측 노드를 채워주면서 부모 노드의 값도 채우기
-    max_tree[index] = max(left, right)
-    return max_tree[index]
+def bfs():
+    que = deque([(0, 0)])
+
+    visited = [[-1] * N for _ in range(N)]
+
+    visited[0][0] = arr[0][0]
+
+    while que:
+        i, j = que.popleft()
+
+        for di, dj in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            ni, nj = i + di, j + dj
+
+            if 0 <= ni < N and 0 <= nj < N:
+                if visited[ni][nj] < 0 or visited[ni][nj] > visited[i][j] + arr[ni][nj]:
+                    que.append((ni, nj))
+                    visited[ni][nj] = visited[i][j] + arr[ni][nj]
+
+    print('---')
+    for i in range(N):
+        print(visited[i])
+    print('---')
+
+    return visited[N - 1][N - 1]
 
 
-# 최솟값 트리 생성 함수
-def min_init(start, end, index):
-    # 가장 끝에 도달했으면 값 배정
-    if start == end:
-        min_tree[index] = integers[start]
-        return min_tree[index]
-    # 도달하지 않았다면 좌우 나누기
-    mid = (start + end) // 2
-    left = min_init(start, mid, index * 2)
-    right = min_init(mid + 1, end, (index * 2) + 1)
-    # 좌측 노드와 우측 노드를 채워주면서 부모 노드의 값도 채우기
-    min_tree[index] = min(left, right)
-    return min_tree[index]
+tc = 0
 
+while True:
+    N = int(input())
 
-# 최댓값 구하는 함수
-def maximum(start, end, index, left, right):
-    # 범위 밖에 있는 경우
-    if left > end or right < start:
-        return 0
-    # 범위 안에 있는 경우
-    if left <= start and right >= end:
-        return max_tree[index]
-    # 그렇지 않다면 두 부분으로 나누기
-    mid = (start + end) // 2
-    # start와 end가 변하면서 최댓값 찾기
-    return max(maximum(start, mid, index * 2, left, right), maximum(mid + 1, end, (index * 2) + 1, left, right))
+    if not N:
+        break
 
+    tc += 1
 
-# 최솟값 구하는 함수
-def minimum(start, end, index, left, right):
-    # 범위 밖에 있는 경우
-    if left > end or right < start:
-        return 10e10
-    # 범위 안에 있는 경우
-    if left <= start and right >= end:
-        return min_tree[index]
-    # 그렇지 않다면 두 부분으로 나누기
-    mid = (start + end) // 2
-    # start와 end가 변하면서 최댓값 찾기
-    return min(minimum(start, mid, index * 2, left, right), minimum(mid + 1, end, (index * 2) + 1, left, right))
-
-
-# 정수 개수 N, a, b의 쌍 수 M
-N, M = map(int, input().split())
-# N 개의 정수
-integers = []
-for _ in range(N):
-    integer = int(input())
-    integers.append(integer)
-
-# 트리 생성(배열의 개수가 N개일 때, N보다 큰 가장 가까운 N의 제곱수를 구한 뒤에 그것의 2배)
-# 넉넉하게 만들기
-max_tree = [0] * ((400 ** 2) * 2)
-min_tree = [10e10] * ((400 ** 2) * 2)
-
-# 트리에 값 부여
-max_init(0, N - 1, 1)
-min_init(0, N - 1, 1)
-print(min_tree[:40])
-
-# M 개의 a, b 쌍마다 구하기
-for _ in range(M):
-    a, b = map(int, input().split())
-
-    ans_min = minimum(0, N - 1, 1, a - 1, b - 1)
-    ans_max = maximum(0, N - 1, 1, a - 1, b - 1)
-    print(ans_min, ans_max)
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    print(f'Problem {tc}: {bfs()}')
