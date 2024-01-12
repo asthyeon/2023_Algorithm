c86bff9eeb3f11097ed75c182974a8b9598eed38
asthyeon <asthyeon@gmail.com>
2024-01-12 11:28:11 +0900

    0112

--- 0_Test/0_Test.py
+++ 0_Test/0_Test.py
@@ -3,83 +3,102 @@ sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
 """
-# 텀 프로젝트
-1. 프로젝트 팀을 만들기 위해 모든 학생등를 함께하고 싶은 학생들을 선택해야 함
- - 프로젝트 팀원 수에는 제한 X(모든 학생들이 동일한 팀의 팀원인 경우 한 팀만 만들어짐)
- - 한 명당 한 명만 선택 가능
- - 혼자 하고 싶어하는 학생은 자기 자신을 선택하는 것도 가능
-2. 주어진 선택의 결과를 보고 어느 프로젝트 팀에도 속하지 않는 학생들의 수를 계산하기
+# 숨바꼭질
+1. 수빈이는 걷거나 순간이동 가능
+ - 수빈이의 위치가 X일 때 걷는다면 1초후 X - 1 or X + 1로 이동
+ - 순간이동한다면 1초 후에 2 * X의 위치로 이동
+2. 수빈이가 동생을 찾을 수 있는 가장 빠른 시간이 몇 초 후인지 구하기
 * 입력
-- 첫째 줄 T: 테스트 케이스의 개수
-- 각 테스트의 첫째 줄 n: 학생의 수
-- 각 테스트의 둘째 줄: 선택된 학생들의 번호가 주어짐
-[출력: 각 줄에 프로젝트 팀에 속하지 못한 학생들의 수 나타내기]
+- N: 수빈이 위치, K: 동생 위치
+[출력: 첫째 줄에 수빈이가 동생을 찾는 가장 빠른 시간, 둘째 줄에 어떻게 이동해야 하는지 출력]
 """
 
 """
 @ 풀이
-(1) 예시 이해해보기
-1 -> 3 X
-2 -> 1 X
-3 -> 3 (3)팀
-4 -> 7 (4, 6, 7)팀
-5 -> 3 X
-6 -> 4 (4, 6, 7)팀
-7 -> 6 (4, 6, 7)팀
-(2) dfs로 탐색
-(3) 시간초과 지옥 -> 재귀로 풀기
+(1) 다익스트라 이용 => 힙큐말고 덱 사용(bfs)
+(2) 이동한 루트를 저장해보기 => q에 저장하면 시간초과
+(3) 하나의 루트에 이동 경로를 저장하고 추적하는 방식으로 해보기
 """
-sys.setrecursionlimit(10**9)
+from collections import deque
 
 
-# dfs 함수
-def dfs(start, project):
-    # 숫자 사용
-    used[start] = 1
-    # 이번 팀 멤버 추가
-    team.append(start)
-    # 이동
-    start = choices[start]
+# bfs
+def bfs(N, K):
+    # 힙큐 생성 및 시작점 인큐(시간, 시작점)
+    pq = deque([(0, N)])
+    # 각 지역마다 필요한 시간 리스트
+    times = [10e9] * 100001
 
-    # 사용한 번호인지 체크
-    if used[start] == 1:
-        # 싸이클이라면 싸이클 시작 지점부터 프로젝트 참여자에 추가
-        if start in team:
-            project += team[team.index(start):]
-    # 사용한 번호가 아니라면 재귀
-    else:
-        dfs(start, project)
+    while pq:
+        # 시간, 현재 위치 디큐
+        time, now = pq.popleft()
+
+        # 동생을 찾았다면 종료
+        if now == K:
+            answer = moved(time, now)
+            return time, answer
+
+        # 이동
+        new_time = time + 1
+        # 좌측 이동
+        if now - 1 >= 0:
+            new = now - 1
+            # 방문하지 않았다면
+            if times[new] == 10e9:
+                times[new] = new_time
+                # 이전 위치 저장
+                passed[new] = now
+                pq.append((new_time, new))
+                # 현재 위치가 동생을 지나친 상태라면 좌측 이동만
+                if now > K:
+                    continue
+
+        # 우측 이동
+        if now + 1 < 100001:
+            new = now + 1
+            # 방문하지 않았다면
+            if times[new] == 10e9:
+                times[new] = new_time
+                # 이전 위치 저장
+                passed[new] = now
+                pq.append((new_time, new))
+
+        # 순간이동
+        if now * 2 <= 100001:
+            new = now * 2
+            # 방문하지 않았다면
+            if times[new] == 10e9:
+                times[new] = new_time
+                # 이전 위치 저장
+                passed[new] = now
+                pq.append((new_time, new))
 
 
-# 테스트 케이스 수
-T = int(input())
-for tc in range(1, T + 1):
-    # 학생 수 n
-    n = int(input())
-    # 학생들이 선택한 번호(편의를 위해 0번 추가)
-    choices = [0]
-    choices += list(map(int, input().split()))
-    # cnt, use = 0, 0
-    # 팀 프로젝트 참여자들
-    project = []
-    # 사용된 숫자 리스트
-    used = [0] * (n + 1)
-    # dfs 탐색
-    for start in range(1, n + 1):
-        if used[start] == 0:
-            # 이번 팀
-            team = []
-            dfs(start, project)
-            # use += 1
-            # print(f'호출: {use}')
-    # print(project)
-    print(n - len(project))
+# 이동 경로 찾기
+def moved(time, location):
+    # 이동경로를 저장할 리스트
+    path = []
+    # 소요된 시간만큼 경로 저장
+    for _ in range(time + 1):
+        # 위치 저장
+        path.append(location)
+        # 위치 저장 후 어디서 왔는지 뒤로 가기
+        location = passed[location]
+    
+    # 방향을 바꿔서 반환
+    return path[::-1]
 
 
+# 수빈이 위치 N, 동생 위치 K
+N, K = map(int, input().split())
 
+# 이동 경로를 저장할 리스트
+passed = [-1] * 100001
+# 소요시간 및 경로 받기
+spend, answer = bfs(N, K)
 
-
-
+print(spend)
+print(*answer)
 
 
 
--- 0_Test/jj_test.py
+++ 0_Test/jj_test.py
@@ -1,38 +1,85 @@
 import sys
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
-sys.setrecursionlimit(111111)  # 충분한 재귀 깊이를 주어 오류를 예방
 
 
-def dfs(x):
-    global cnt
-    cnt += 1
-    print(f'사용횟수: {cnt}')
-    global result
-    visited[x] = True
-    cycle.append(x)  # 사이클을 이루는 팀을 확인하기 위함
-    number = numbers[x]
+"""
+# 숨바꼭질
+1. 수빈이는 걷거나 순간이동 가능
+ - 수빈이의 위치가 X일 때 걷는다면 1초후 X - 1 or X + 1로 이동
+ - 순간이동한다면 1초 후에 2 * X의 위치로 이동
+2. 수빈이가 동생을 찾을 수 있는 가장 빠른 시간이 몇 초 후인지 구하기
+* 입력
+- N: 수빈이 위치, K: 동생 위치
+[출력: 첫째 줄에 수빈이가 동생을 찾는 가장 빠른 시간, 둘째 줄에 어떻게 이동해야 하는지 출력]
+"""
 
-    if visited[number]:  # 방문가능한 곳이 끝났는지
-        if number in cycle:  # 사이클 가능 여부
-            result += cycle[cycle.index(number):]  # 사이클 되는 구간 부터만 팀을 이룸
-        return
-    else:
-        dfs(number)
+"""
+@ 풀이
+(1) 다익스트라 이용
+(2) 이동한 루트를 저장해보기
+(3) N이 K보다 큰 경우는 따로 만들기
+"""
+import heapq
 
 
-for _ in range(int(input())):
-    N = int(input())
-    numbers = [0] + list(map(int, input().split()))
-    visited = [True] + [False] * N  # 방문 여부
-    result = []
-    # 사용횟수
-    cnt, use = 0, 0
-    for i in range(1, N + 1):
-        if not visited[i]:  # 방문 안한 곳이라면
-            cycle = []
-            dfs(i)  # DFS 함수 돌림
-            use += 1
-            print(f'호출: {use}')
+# 다익스트라
+def dijkstra(N, K):
+    # N보다 K가 큰 경우는 뒤로 가기만 하면 되므로 새로 리스트 만들기
+    if N > K:
+        # 시간은 N - K, 이동 경로는 하나씩 증가하면 됨
+        return N - K, [loc for loc in range(N, K - 1, -1)]
+    # N이 K와 같은 경우는 바로 끝내기
+    elif N == K:
+        return 0, [N]
 
-    print(N - len(result))  # 팀에 없는 사람 수
+    # 힙큐 생성 및 시작점 인큐(시간, 시작점)
+    hq = [(0, [N])]
+    # 각 지역마다 필요한 시간 리스트
+    times = [10e9] * 100001
+
+    while hq:
+        # 시간, 이동루트 디큐
+        time, routes = heapq.heappop(hq)
+
+        # 동생을 찾았다면 종료
+        if routes[-1] == K:
+            return time, routes
+
+        # 방문할 지역이 시간이 더 짧은 상태로 방문했다면 넘기기
+        if times[routes[-1]] <= time:
+            continue
+
+        # 방문처리
+        times[routes[-1]] = time
+
+        # 이동
+        new_time = time + 1
+        # 좌측 이동
+        if routes[-1] > 0:
+            # 시간
+            new = routes[-1] - 1
+            heapq.heappush(hq, (time + 1, routes + [new]))
+            # 현재 위치가 동생을 지나친 상태라면 좌측 이동만
+            if routes[-1] > K:
+                continue
+
+        # 우측 이동
+        if routes[-1] < 100000:
+            new = routes[-1] + 1
+            heapq.heappush(hq, (time + 1, routes + [new]))
+
+        # 순간이동
+        if routes[-1] <= 50000:
+            new = routes[-1] * 2
+            heapq.heappush(hq, (time + 1, routes + [new]))
+
+
+# 수빈이 위치 N, 동생 위치 K
+N, K = map(int, input().split())
+
+# 소요시간과 이동 루트 받기
+spend, moved = dijkstra(N, K)
+
+print(spend)
+print(*moved)
